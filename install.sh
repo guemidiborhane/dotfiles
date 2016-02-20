98b17be47bed07c5b9462e6cb3cd12204d265190
Borhane Eddine Guemidi <guemidiborhane@gmail.com>
2016-02-20 01:24:35 +0100

    create symlinks for themes

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 dir=$(pwd)
 
-ln -fs $dir/.zsh_auto_complete $HOME/ 
-ln -fs $dir/.aliases $HOME/ 
-ln -fs $dir/.zshrc $HOME/ 
-ln -fs $dir/.tmux.conf $HOME/ 
+ln -fsv $dir/.zsh_auto_complete $HOME
+ln -fsv $dir/.aliases $HOME
+ln -fsv $dir/.zshrc $HOME
+ln -fsv $dir/.tmux.conf $HOME
+
+ln -fsv $dir/.oh-my-zsh/themes/* $HOME/.oh-my-zsh/themes 
