fe17bbb3ef7e6094aab6f20730574ac7dcca3a95
Borhane E. Guemidi <guemidiborhane@gmail.com>
2016-09-29 22:47:08 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ dir=$(pwd)
 
 ln -fsv $dir/.zsh_auto_complete $HOME
 ln -fsv $dir/.aliases $HOME
-ln -fsv $dir/.zshrc $HOME
+ln -fsv $dir/.*rc $HOME
 ln -fsv $dir/.tmux.conf $HOME
+ln -fsv $dir/.gitconfig $HOME
 
-ln -fsv $dir/.oh-my-zsh/themes/* $HOME/.oh-my-zsh/themes 
+ln -fsv $dir/.oh-my-zsh/themes/* $HOME/.oh-my-zsh/themes
 ln -fsv $dir/bin $HOME/.bin
