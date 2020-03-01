6654a77b1a290e08273807548a875797d7c6de77
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2020-03-01 01:00:56 +0100

    Add regolith-i3 config files

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ ln -fsv $dir/bin $HOME/.bin
 ln -fsv $dir/.vim $HOME
 ln -fsv $dir/.tmuxinator $HOME
 ln -fsv $dir/p10k.zsh $HOME/.p10k.zsh
+ln -fsv $dir/.Xresources-* $HOME
 
 mkdir $HOME/Code
 ln -fsv $dir/Code/key_ok.png $HOME/Code
