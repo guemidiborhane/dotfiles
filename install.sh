05929c68398e050993e5dc2b8f925466ce49ab22
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2020-03-14 22:41:41 +0100

    Remove dots from names

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 dir=$(pwd)
 
-ln -fsv $dir/.zsh_auto_complete $HOME
-ln -fsv $dir/.aliases $HOME
-ln -fsv $dir/.*rc $HOME
-ln -fsv $dir/.tmux.conf $HOME
-ln -fsv $dir/.gitconfig $HOME
+ln -fsv $dir/zshrc $HOME/.zshrc
+ln -fsv $dir/vimrc $HOME/.vimrc
+ln -fsv $dir/zsh_auto_complete $HOME/.zsh_auto_complete
+ln -fsv $dir/aliases $HOME/.aliases
+ln -fsv $dir/gemrc $HOME/.gemrc
+ln -fsv $dir/railsrc $HOME/.railsrc
+ln -fsv $dir/tmux.conf $HOME/.tmux.conf
+ln -fsv $dir/gitconfig $HOME/.gitconfig
 
-ln -fsv $dir/.oh-my-zsh/themes/* $HOME/.oh-my-zsh/themes
 ln -fsv $dir/bin $HOME/.bin
-ln -fsv $dir/.vim $HOME
-ln -fsv $dir/.tmuxinator $HOME
+ln -fsv $dir/vim $HOME/.vim
 ln -fsv $dir/p10k.zsh $HOME/.p10k.zsh
-ln -fsv $dir/.Xresources-* $HOME
+ln -fsv $dir/.Xresources $HOME
 
 mkdir $HOME/Code
 ln -fsv $dir/Code/key_ok.png $HOME/Code
