c6001082ace394b843c294abad06e2e0010267d8
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2020-04-17 04:39:24 +0100

    Fetch location using multiple APIs

--- config/i3blocks/openweather.sh
+++ config/i3blocks/openweather.sh
@@ -5,10 +5,13 @@ Aborting."; exit 1; }
 command -v wget >/dev/null 2>&1 || { echo >&2 "Program 'wget' required but is not installed.  
 Aborting."; exit 1; }
 
+IP=`wget -qO- https://ipecho.net/plain`
+IPSTACK_APIKEY="2429834f88ef2fa1a9364f74c8292b69"
+CITY_NAME=$(wget -qO- "http://api.ipstack.com/${IP}?access_key=${IPSTACK_APIKEY}&format=1" | jq '.city' | sed 's/"//g')
+
 APIKEY="7e366d360f98786b7286d6ee425d5287"
 #ZIPCODE="1234"
-CITY_ID="2507480"
-URL="http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&units=metric&APPID=${APIKEY}"
+URL="http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&APPID=${APIKEY}"
 
 WEATHER_RESPONSE=$(wget -qO- "${URL}")
 
