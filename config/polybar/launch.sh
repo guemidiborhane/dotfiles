66b00686ce4851fca202e87a157d61d2fdf5ccdb
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2021-05-28 03:42:09 +0100

    :poop: Fix trayicon position (Hopefully)

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,13 +8,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-polybar -r center &
-
+# Launch bars
 external_monitor=$(xrandr --query | grep 'DP-1-1')
 if [[ $external_monitor = *connected* ]]; then
       polybar -r external &
 fi
+
+polybar -r center &
+
 hdmi_monitor=$(xrandr --query | grep 'HDMI-1-0')
 if [[ $external_monitor = *connected* ]]; then
       polybar -r hdmi &
