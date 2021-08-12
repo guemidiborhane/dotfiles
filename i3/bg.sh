9154f75d2d4158cd1a95310950e11e201355ec42
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2021-08-12 12:30:46 +0100

    :bug: Fix feh not knowing DISPLAY
    + Add notification

--- i3/bg.sh
+++ i3/bg.sh
@@ -4,4 +4,5 @@ files=(~/Pictures/wallpapers/*)
 WALLPAPER1=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER3=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-feh -B white --bg-fill "$WALLPAPER1" --bg-fill "$WALLPAPER2" --bg-fill "$WALLPAPER3"
+DISPLAY=:0 feh -B white --bg-fill "$WALLPAPER1" --bg-fill "$WALLPAPER2" --bg-fill "$WALLPAPER3"
+# XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u low "wallpaper changed"
