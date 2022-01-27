c7644416f621015233f10906de8a2b893958e985
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-01-27 17:16:58 +0100

    :hammer: Tweak and remove unnecessary plugins

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -12,22 +12,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 BAR_NAME=main
 BAR_CONFIG=/home/$USER/.config/polybar/config
 
-BUILT_IN="eDP-1"
-MAIN_EXTER="HDMI-1-0"
-SECONDARY_EXTER="DP-1-1"
+MAIN_EXTER="HDMI-0"
 
 external_monitor=$(xrandr --query | grep $MAIN_EXTER)
 if [[ $external_monitor = *connected* ]]; then
     MONITOR=$MAIN_EXTER TRAY_POS="right" polybar --reload -c $BAR_CONFIG $BAR_NAME &
     sleep 1
-    MONITOR=$BUILT_IN TRAY_POS="" polybar --reload -c $BAR_CONFIG $BAR_NAME &
-    sleep 1
-else
-    MONITOR=$BUILT_IN TRAY_POS="right" polybar --reload -c $BAR_CONFIG $BAR_NAME &
-    sleep 1
-fi
-
-hdmi_monitor=$(xrandr --query | grep $SECONDARY_EXTER)
-if [[ $external_monitor = *connected* ]]; then
-    MONITOR=$SECONDARY_EXTER TRAY_POS="" polybar --reload -c $BAR_CONFIG $BAR_NAME &
 fi
