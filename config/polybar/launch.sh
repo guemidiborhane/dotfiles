1b3738fcb824867f7c830ec551db6dfbf038f9bf
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-02-08 14:06:23 +0100

    :recycle: Dynamic monitors detection for polybar launch

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -12,10 +12,20 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 BAR_NAME=main
 BAR_CONFIG=/home/$USER/.config/polybar/config
 
-MAIN_EXTER="HDMI-0"
+DISPLAYS=$(xrandr --query | grep ' connected' | awk '{print $1}')
 
-external_monitor=$(xrandr --query | grep $MAIN_EXTER)
-if [[ $external_monitor = *connected* ]]; then
-    MONITOR=$MAIN_EXTER TRAY_POS="right" polybar --reload -c $BAR_CONFIG $BAR_NAME &
+for monitor in ${DISPLAYS[@]}; do
+  external_monitor=$(xrandr --query | grep $monitor)
+  if [[ $external_monitor = *connected* ]]; then
+
+    if [[ $external_monitor = *primary* ]]; then
+        MONITOR=$monitor TRAY_POS="right" polybar --reload -c $BAR_CONFIG $BAR_NAME &
+        echo "Primary monitor connected: $monitor"
+    else
+        MONITOR=$monitor TRAY_POS="" polybar --reload -c $BAR_CONFIG $BAR_NAME &
+    fi
+
     sleep 1
-fi
+  fi
+done
+
