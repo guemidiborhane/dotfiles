9327640c046b27580a42b8ac9b308396369becde
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-02-09 13:09:16 +0100

    :recycle: Use `--randomize` flag for feh

--- i3/bg.sh
+++ i3/bg.sh
@@ -1,8 +1,4 @@
 
 #!/usr/bin/bash
-files=(~/Pictures/wallpapers/*)
-WALLPAPER1=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-WALLPAPER2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-WALLPAPER3=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-DISPLAY=:0 feh -B white --bg-fill "$WALLPAPER1" --bg-fill "$WALLPAPER2" --bg-fill "$WALLPAPER3"
+DISPLAY=:0 feh --randomize --bg-scale --no-xinerama ~/Pictures/wallpapers/*
 # XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u low "wallpaper changed"
