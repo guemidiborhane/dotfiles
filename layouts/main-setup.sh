1deff6fdfbd0ad9533cd18b75c2e83df0d7c4b92
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-02-12 15:06:32 +0100

    :hammer: Add screens layout script

---
+++ layouts/main-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 2560x1440 --pos 4000x615 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x615 --rotate normal --output DP-5 --off
