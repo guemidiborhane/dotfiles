ba499b1b7842267353d8c5974408a8105cfa37ae
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-02-15 23:35:49 +0100

    :fire: Remove last saved workspaces

--- i3/scripts/save-workspaces.sh
+++ i3/scripts/save-workspaces.sh
@@ -2,6 +2,8 @@
 
 workspaces=$(i3-msg -t get_workspaces | jq '.[].name' | cut -d"\"" -f2)
 
+rm -f $HOME/.i3/i3-resurrect/workspace_*.json
+
 for workspace in $workspaces; do
     i3-resurrect save -w $workspace
     echo "Saved workspace $workspace"
