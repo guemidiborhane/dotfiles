f4c89e1cd2b8b0510ee0ff26983c34614e897999
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-03-04 11:42:23 +0100

    :wrench: Update screens layouts script path

--- layouts/main-setup.sh
+++ layouts/main-setup.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 4000x615 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x615 --rotate normal --output DP-5 --off
+xrandr \
+    --output DP-0 --mode 2560x1440 --pos 4000x615 --rotate normal --rate 74.97 \
+    --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --rate 74.97 \
+    --output DP-4 --primary --mode 2560x1440 --pos 1440x615 --rotate normal --rate 74.97
