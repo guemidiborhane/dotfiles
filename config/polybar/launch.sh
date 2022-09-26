24b7b6dbe67cbb6adf787063bae6fa66b82346c6
Borhaneddine Guemidi <guemidiborhane@aina.dz>
2022-09-26 23:43:32 +0100

    :truck: Split polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -9,14 +9,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-BAR_CONFIG=/home/$USER/.config/polybar/config
+BAR_CONFIG=/home/$USER/.config/polybar/config.ini
 
 DISPLAYS=$(xrandr --query | grep ' connected' | awk '{print $1}')
 
 for monitor in ${DISPLAYS[@]}; do
   external_monitor=$(xrandr --query | grep $monitor)
   if [[ $external_monitor = *connected* ]]; then
-    MONITOR=$monitor polybar --reload -c $BAR_CONFIG $monitor 2>&1 | tee -a /tmp/polybar-$monitor.log & disown
+    MONITOR=$monitor polybar -c $BAR_CONFIG $monitor 2>&1 | tee -a /tmp/polybar-$monitor.log & disown
     sleep 1
   fi
 done
