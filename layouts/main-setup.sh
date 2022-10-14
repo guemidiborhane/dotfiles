4227437b51ec981e421f2de80473681662176c8c
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2022-10-14 21:06:09 +0100

    :bento: Clean things up

--- layouts/main-setup.sh
+++ layouts/main-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr \
-    --output DP-0 --mode 2560x1440 --pos 4000x615 --rotate normal --rate 59.95 \
+    --output DP-0 --mode 2560x1440 --pos 4000x560 --rotate normal --rate 59.95 \
     --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --rate 59.95 \
-    --output DP-4 --primary --mode 2560x1440 --pos 1440x615 --rotate normal --rate 59.95
+    --output DP-4 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --rate 59.95
