e5ce5c41ff7217ad879fa2c2919c94db7948741e
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-01-21 19:12:56 +0100

    :broom: Spring cleanup

--- layouts/main-setup.sh
+++ layouts/main-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr \
-    --output DP-0 --mode 2560x1440 --pos 4000x560 --rotate normal --rate 74.97 \
-    --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --rate 74.97 \
-    --output DP-4 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --rate 74.97
+    --output DP-0 --mode 2560x1440 --pos 0x560 --rotate normal --rate 74.97 \
+    --output DP-2 --mode 2560x1440 --pos 5120x0 --rotate right --rate 74.97 \
+    --output DP-4 --primary --mode 2560x1440 --pos 2560x560 --rotate normal --rate 74.97
