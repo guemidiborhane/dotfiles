56b30eff314b34223ee3f7d46e2ae312dcca011a
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-02-02 17:43:30 +0100

    :truck: Move utilities to utils.sh

--- .config/yadm/scripts/utils.sh
+++ .config/yadm/scripts/utils.sh
@@ -9,3 +9,19 @@ ask () {
     read -p "$1? [y/N] " -n 1 -r
     echo   # (optional) move to a new line
 }
+
+section () {
+    echo
+    echo "===================="
+    echo " $1"
+    echo "===================="
+    echo
+}
+
+readme () {
+echo <<EOF
+    REMINDER:
+    - Don't forget to run `chsh -s $(which zsh)` to set zsh as your default shell
+    - You should probably reboot now ! just to be sure
+EOF
+}
