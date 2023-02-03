777a2d75e059d176bac9ea8895510d8568fe7215
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-02-03 15:04:52 +0100

    :bug: Fix polybar bars configuration

--- .config/yadm/scripts/polybar.sh
+++ .config/yadm/scripts/polybar.sh
@@ -23,17 +23,17 @@ EOF
 touch_polybar_bars_ini () {
     if [[ ! -f $bars_ini_path ]]; then
         echo "Creating $bars_ini_path"
-        echo _default_content > $bars_ini_path
+        _default_content > $bars_ini_path
     fi
 }
 
 configure_polybar () {
     if [[ ! -f $bars_ini_path ]]; then
-        echo "Creating $bars_ini_path"
+        echo "Adding default bars to $bars_ini_path"
         touch_polybar_bars_ini
         DISPLAYS=$(xrandr --query | grep ' connected' | awk '{print $1}')
         for monitor in ${DISPLAYS[@]}; do
-            echo _bar_content $monitor >> $bars_ini_path
+            _bar_content $monitor >> $bars_ini_path
         done
     fi
 }
