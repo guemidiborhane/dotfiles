aacb1cc4e3613a6fa5326a22989b9591fbd5b540
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-02-04 17:56:07 +0100

    :wheelchair: Add hints

--- .config/yadm/bootstrap.d/mega.sh
+++ .config/yadm/bootstrap.d/mega.sh
@@ -2,14 +2,16 @@
 # vim: set ft=sh sw=4 et :
 
 install_sync () {
+    echo "Installing MegaSync"
     curl -s https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst -o /tmp/megasync-x86_64.pkg.tar.zst
-    sudo pacman -U /tmp/megasync-x86_64.pkg.tar.zst
+    sudo pacman -U --noconfirm /tmp/megasync-x86_64.pkg.tar.zst
     rm /tmp/megasync-x86_64.pkg.tar.zst
 }
 
 install_megacmd () {
+    echo "Installing MegaCMD"
     curl -s https://mega.nz/linux/MEGAsync/Arch_Extra/x86_64/megacmd-x86_64.pkg.tar.zst -o /tmp/megacmd-x86_64.pkg.tar.zst
-    sudo pacman -U /tmp/megacmd-x86_64.pkg.tar.zst
+    sudo pacman -U --noconfirm /tmp/megacmd-x86_64.pkg.tar.zst
     rm /tmp/megacmd-x86_64.pkg.tar.zst
 }
 
