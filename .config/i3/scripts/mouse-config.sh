512b02bd8c5583a4f44c1d9a7d6f77755064ced9
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-02-16 07:16:27 +0100

    :wrench: Update mouse configs

--- .config/i3/scripts/mouse-config.sh
+++ .config/i3/scripts/mouse-config.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # vim: set ft=sh sw=4 et :
 
-solaar config 1 dpi 500
+solaar config 1 dpi 800
 xinput set-prop "pointer:Logitech MX Master 3" "libinput Accel Speed" -0.5
+xinput set-prop "pointer:Logitech MX Master 3" 'libinput Accel Profile Enabled' 0, 1
+
