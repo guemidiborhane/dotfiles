104a8795a635bce6273d2b61ba139955d1da80cc
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-03-16 03:46:37 +0100

    :bug: Autoload imput-remapper on start

--- .config/i3/scripts/keyboard-layout.sh
+++ .config/i3/scripts/keyboard-layout.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+input-remapper-control --command stop-all && input-remapper-control --command autoload
+notify-send "input remapper autoloaded"
 
 setxkbmap -layout fr -variant us-azerty
 notify-send "Keyboard layout set"
