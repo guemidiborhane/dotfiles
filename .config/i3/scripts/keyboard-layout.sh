ea8e127bef7ab846e51e2cfe62dcf07300e78d83
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-03-26 20:53:05 +0100

    :bug: Fix input remapper autoload
    We should wait before setting the keyboard layout

--- .config/i3/scripts/keyboard-layout.sh
+++ .config/i3/scripts/keyboard-layout.sh
@@ -2,5 +2,7 @@
 input-remapper-control --command stop-all && input-remapper-control --command autoload
 notify-send "input remapper autoloaded"
 
+sleep 2
+
 setxkbmap -layout fr -variant us-azerty
 notify-send "Keyboard layout set"
