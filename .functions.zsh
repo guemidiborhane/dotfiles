e2746b40dcd7cef644f2c193d0e8ae0e08d6c8ca
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-03-26 20:53:24 +0100

    :bug: Fix pagespeed function

--- .functions.zsh
+++ .functions.zsh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function pagespeed() {
-    curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nResponse Code:\t\t%{http_code}\nCookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null --connect-timeout 1 $1
+    curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nResponse Code:\t\t%{response_code}\nCookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null --connect-timeout 1 $1
 }
 
 function kickoff() {
