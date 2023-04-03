4d6d8d9e2b0fb0319ea1a06f79366f4f96c42685
Borhaneddine  GUEMIDI <guemidiborhane@gmail.com>
2023-04-03 03:23:40 +0100

    :fire: Remove xf86-video-intel dependency
    It causes screen tearing on the laptop

--- .config/yadm/bootstrap.d/system.sh
+++ .config/yadm/bootstrap.d/system.sh
@@ -13,7 +13,6 @@ systemctl restart NetworkManager
 EOF
 sudo tee /etc/resolv.conf &>/dev/null <<EOF
 # bootstraped by yadm
-options rotate timeout:1 retries:1
 nameserver 127.0.0.1
 nameserver 1.1.1.1
 nameserver 1.0.0.1
@@ -22,6 +21,26 @@ nameserver 1.0.0.1
 nameserver 2111:3c:123:0:c:135:9a:a15
 nameserver 2111:3c:123:0:3bc6:a:9cc:518
 EOF
+}
+
+configure_backlight_udev_rule () {
+sudo tee /etc/udev/rules.d/90-backlight.rules &>/dev/null <<EOF
+ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"
+EOF
+    sudo usermod -aG video $USER
+}
+
+remove_xf86_video_intel () {
+    sudo pacman -R --noconfirm xf86-video-intel
+}
+
+configure_backlight () {
+    ask "Configure backlight"
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        remove_xf86_video_intel
+        configure_backlight_udev_rule
+    fi
 }
 
 configure_resolvconf () {
