82d313d8bb22f5e416e6de8d1c3697ba6f304a34
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-04 13:19:50 +0100

    :wrench: Make it feel more like home

--- .config/custom-nvim/init.lua
+++ .config/custom-nvim/init.lua
@@ -6,3 +6,13 @@ opt.shiftwidth = 4
 opt.smartindent = true
 opt.tabstop = 4
 opt.softtabstop = 4
+
+if vim.g.neovide then
+    vim.o.guifont = "Operator Mono Book:h14"
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_cursor_animation_length = 0.05
+end
--- .config/custom-nvim/plugins.lua
+++ .config/custom-nvim/plugins.lua
@@ -1,37 +1,47 @@
 local plugins = {
-  {
-    'github/copilot.vim',
-    lazy = false
-  },
+    {
+        'github/copilot.vim',
+        lazy = false
+    },
 
-  {
-    'neovim/nvim-lspconfig',
-    config = function ()
-      require 'plugins.configs.lspconfig'
-      require 'custom.configs.lspconfig'
-    end
-  },
+    {
+        'neovim/nvim-lspconfig',
+        config = function ()
+          require 'plugins.configs.lspconfig'
+          require 'custom.configs.lspconfig'
+        end
+    },
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        'solargraph'
-      }
+    {
+        "williamboman/mason.nvim",
+        opts = {
+          ensure_installed = {
+            'solargraph'
+          }
+        }
+    },
+
+    {
+        "NvChad/nvterm",
+        init = function()
+          require("core.utils").load_mappings "nvterm"
+        end,
+        config = function(_, opts)
+          -- require "base46.term"
+          require("nvterm").setup(opts)
+          require 'custom.configs.nvterm'
+        end,
+    },
+
+    {
+        "ahmedkhalf/project.nvim",
+        lazy = false,
+        config = function()
+            require("project_nvim").setup()
+            require 'custom.configs.tree'
+            require 'custom.configs.telescope'
+        end
     }
-  },
-
-  {
-    "NvChad/nvterm",
-    init = function()
-      require("core.utils").load_mappings "nvterm"
-    end,
-    config = function(_, opts)
-      require "base46.term"
-      require("nvterm").setup(opts)
-      require 'custom.configs.nvterm'
-    end,
-  },
 }
 
 return plugins
