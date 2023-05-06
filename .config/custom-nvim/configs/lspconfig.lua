8ef52655c35dc79b681be49a8c288fe77521d108
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-06 15:05:09 +0100

    :hammer: Add Emmet to neovim

--- .config/custom-nvim/configs/lspconfig.lua
+++ .config/custom-nvim/configs/lspconfig.lua
@@ -1,11 +1,2 @@
-local on_attach = require('plugins.configs.lspconfig').on_attach
-local capabilities = require('plugins.configs.lspconfig').capabilities
-
-local lspconfig = require 'lspconfig'
-
-lspconfig.solargraph.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {'ruby'},
-  root_dir = lspconfig.util.root_pattern("Gemfile", ".git")
-})
+require 'custom.configs.lsp.solargraph'
+require 'custom.configs.lsp.emmet'
