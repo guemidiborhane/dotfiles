d153b186dc0f86dfa535b67966b4f265a603158c
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-07 18:43:20 +0100

    :wrench: Switch to ruby-lsp

--- .config/custom-nvim/configs/lspconfig.lua
+++ .config/custom-nvim/configs/lspconfig.lua
@@ -1,2 +1,14 @@
-require 'custom.configs.lsp.solargraph'
-require 'custom.configs.lsp.emmet'
+require("custom.configs.lsp.emmet")
+
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local servers = { "ruby_ls" }
+
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+    })
+end
