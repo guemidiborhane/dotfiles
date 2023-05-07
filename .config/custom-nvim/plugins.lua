bd0acde282b5be763ccd4edfec5ffb36abf04d46
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-07 17:44:08 +0100

    :hammer: Add Null-ls for formatting

--- .config/custom-nvim/plugins.lua
+++ .config/custom-nvim/plugins.lua
@@ -1,35 +1,36 @@
 local plugins = {
     {
-        'Exafunction/codeium.vim',
+        "Exafunction/codeium.vim",
         lazy = false,
     },
 
     {
-        'neovim/nvim-lspconfig',
-        config = function ()
-          require 'plugins.configs.lspconfig'
-          require 'custom.configs.lspconfig'
-        end
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("plugins.configs.lspconfig")
+            require("custom.configs.lspconfig")
+        end,
     },
 
     {
         "williamboman/mason.nvim",
         opts = {
-          ensure_installed = {
-            'solargraph', 'emmet_ls'
-          }
-        }
+            ensure_installed = {
+                "solargraph",
+                "emmet-ls",
+            },
+        },
     },
 
     {
         "NvChad/nvterm",
         init = function()
-          require("core.utils").load_mappings "nvterm"
+            require("core.utils").load_mappings("nvterm")
         end,
         config = function(_, opts)
-          require "base46.term"
-          require("nvterm").setup(opts)
-          require 'custom.configs.nvterm'
+            require("base46.term")
+            require("nvterm").setup(opts)
+            require("custom.configs.nvterm")
         end,
     },
 
@@ -38,10 +39,22 @@ local plugins = {
         lazy = false,
         config = function()
             require("project_nvim").setup()
-            require 'custom.configs.tree'
-            require 'custom.configs.telescope'
-        end
-    }
+            require("custom.configs.tree")
+            require("custom.configs.telescope")
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        lazy = false,
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "jose-elias-alvarez/null-ls.nvim",
+        },
+        config = function()
+            require("custom.configs.null_ls")
+        end,
+    },
 }
 
 return plugins
