a5e0e7e25deaa24db6575caf595aae1826f0df54
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-10 19:58:59 +0100

    :wrench: Add splitting mappings

--- .config/custom-nvim/mappings.lua
+++ .config/custom-nvim/mappings.lua
@@ -13,17 +13,45 @@ local toggle_nvterm = {
     "toggle floating term",
 }
 
+local find_files_command = "<cmd> Telescope find_files <CR>"
+
+local find_files = {
+    find_files_command,
+    "find files",
+}
+
+-- close splits and last window if no splits
+local close_tab = {
+    "<cmd> close <CR>",
+    "close current tab",
+}
+
+local vertical_split = {
+    "<cmd> vsplit <CR>" .. find_files_command,
+    "vertical split",
+}
+
+local horizontal_split = {
+    "<cmd> split <CR>" .. find_files_command,
+    "horizontal split",
+}
+
 M.personal = {
     n = {
-        ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "find files" },
-        ["<C-S-p>"] = { "<cmd> Telescope commands <CR>", "show commands" },
-        ["<C-k>w"] = { "<cmd> :bufdo bd<CR> :Nvdash<CR>", "close all buffers" },
+        ["<C-p>"] = find_files,
+        ["<C-k>w"] = { "<cmd> bufdo bd<CR> <cmd> Nvdash<CR>", "close all buffers" },
         ["<C-`>"] = toggle_nvterm,
         ["<S-A-p>"] = { "<cmd> Telescope projects <CR>" },
-        ["<C-w>"] = { "<cmd> bufdo bd<CR>", "close current buffer" },
+        ["<C-w>"] = close_tab,
+        ["<C-l>"] = vertical_split,
+        ["<C-j>"] = horizontal_split,
     },
     i = {
+        ["<C-p>"] = find_files,
         ["<C-s>"] = { "<cmd> write <CR>", "save" },
+        ["<C-w>"] = close_tab,
+        ["<C-l>"] = vertical_split,
+        ["<C-j>"] = horizontal_split,
     },
     t = {
         ["<C-`>"] = toggle_nvterm,
