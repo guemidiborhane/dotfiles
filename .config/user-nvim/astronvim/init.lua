5cce14a9e71480d675c1844057a41ea92fd179a5
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-16 11:55:07 +0100

    :wrench: Remove trailing whitespaces
    Credit : https://vi.stackexchange.com/a/41388

--- .config/user-nvim/astronvim/init.lua
+++ .config/user-nvim/astronvim/init.lua
@@ -81,5 +81,13 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      pattern = { "*" },
+      callback = function(ev)
+        save_cursor = vim.fn.getpos "."
+        vim.cmd [[%s/\s\+$//e]]
+        vim.fn.setpos(".", save_cursor)
+      end,
+    })
   end,
 }
