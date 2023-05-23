649f752772a416036e110b62f7c63260cdf09805
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-05-23 18:21:17 +0100

    :lipstick: Update polybar icons

--- .config/polybar/scripts/cpu-frequency.sh
+++ .config/polybar/scripts/cpu-frequency.sh
@@ -8,9 +8,10 @@ do
     c=$((c+1))
 done
 
-min_frequency=$(echo "scale=2; $(cat /proc/cpuinfo | grep MHz | sort -n | head -1 | awk '/MHz/ {print $4}') / 1000" | bc)
+# min_frequency=$(echo "scale=2; $(cat /proc/cpuinfo | grep MHz | sort -n | head -1 | awk '/MHz/ {print $4}') / 1000" | bc)
 avg_frequency=$(echo "scale=2; $t / $c / 1000" | bc)
-max_frequency=$(echo "scale=2; $(cat /proc/cpuinfo | grep MHz | sort -n | tail -1 | awk '/MHz/ {print $4}') / 1000" | bc)
+# max_frequency=$(echo "scale=2; $(cat /proc/cpuinfo | grep MHz | sort -n | tail -1 | awk '/MHz/ {print $4}') / 1000" | bc)
 
 
-echo "$min_frequency $avg_frequency $max_frequency" | awk '{print  $2"GHz   | "$1"GHz   " $3 "GHz"}')
+# echo "$min_frequency $avg_frequency $max_frequency" | awk '{print  $2"GHz   | "$1"GHz   " $3 "GHz"}')
+echo $avg_frequency"GHz")
