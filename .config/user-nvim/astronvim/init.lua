16f2db9ddcbd8e153fbae5c2af27c766ceda4e67
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-06-05 17:45:24 +0100

    :wrench: Tweak heirline

--- .config/user-nvim/astronvim/init.lua
+++ .config/user-nvim/astronvim/init.lua
@@ -69,7 +69,6 @@ return {
   icons = {
     VimIcon = "",
     ScrollText = "",
-    GitBranch = "",
     GitAdd = "",
     GitChange = "",
     GitDelete = "",
@@ -79,18 +78,19 @@ return {
     -- define the separators between each section
     separators = {
       left = { "", " " }, -- separator for the left side of the statusline
-      right = { " ", "" }, -- separator for the right side of the statusline
-      tab = { "", "" },
+      right = { " ", "" }, -- separator for the right side of the statusline
+      top_left = { "", "" },
+      top_right = { "", "" },
+      bottom_left = { "", "" },
+      bottom_right = { "", "" },
+      tab = { "", "" },
+      breadcrumbs = "  ",
+      path = "  ",
     },
     -- add new colors that can be used by heirline
     colors = function(hl)
       local get_hlgroup = require("astronvim.utils").get_hlgroup
       -- use helper function to get highlight group properties
-      local comment_fg = get_hlgroup("Comment").fg
-      hl.git_branch_fg = comment_fg
-      hl.git_added = comment_fg
-      hl.git_changed = comment_fg
-      hl.git_removed = comment_fg
       hl.blank_bg = get_hlgroup("Folded").fg
       hl.file_info_bg = get_hlgroup("Visual").bg
       hl.nav_icon_bg = get_hlgroup("String").fg
@@ -126,8 +126,8 @@ return {
     -- }
     vim.api.nvim_create_autocmd({ "BufWritePre" }, {
       pattern = { "*" },
-      callback = function(ev)
-        save_cursor = vim.fn.getpos "."
+      callback = function(_)
+        local save_cursor = vim.fn.getpos "."
         vim.cmd [[%s/\s\+$//e]]
         vim.fn.setpos(".", save_cursor)
       end,
--- .config/user-nvim/astronvim/plugins/heirline.lua
+++ .config/user-nvim/astronvim/plugins/heirline.lua
@@ -1,111 +1,135 @@
+local get_icon = require("astronvim.utils").get_icon
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
     local status = require "astronvim.utils.status"
     opts.statusline = {
-      -- default highlight for the entire statusline
       hl = { fg = "fg", bg = "bg" },
-      -- each element following is a component in astronvim.utils.status module
-
-      -- add the vim mode component
-      status.component.mode {
-        -- enable mode text with padding as well as an icon before it
-        mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 1 } } },
-        -- surround the component with a separators
-        surround = {
-          -- it's a left element, so use the left separator
-          separator = "left",
-          -- set the color of the surrounding based on the current mode using astronvim.utils.status module
-          color = function() return { main = status.hl.mode_bg(), right = "blank_bg" } end,
-        },
-      },
-      -- we want an empty space here so we can use the component builder to make a new section with just an empty string
-      status.component.builder {
-        { provider = "" },
-        -- define the surrounding separator and colors to be used inside of the component
-        -- and the color to the right of the separated out section
-        surround = { separator = "left", color = { main = "blank_bg", right = "file_info_bg" } },
-      },
-      -- add a section for the currently opened file information
-      status.component.file_info {
-        -- enable the file_icon and disable the highlighting based on filetype
-        file_icon = { padding = { left = 0 } },
-        filename = { fallback = "Empty" },
-        -- add padding
-        padding = { right = 1 },
-        -- define the section separator
-        surround = { separator = "left", condition = false },
-      },
-      -- add a component for the current git branch if it exists and use no separator for the sections
-      status.component.git_branch { surround = { separator = "none" } },
-      -- add a component for the current git diff if it exists and use no separator for the sections
-      status.component.git_diff { padding = { left = 1 }, surround = { separator = "none" } },
-      -- fill the rest of the statusline
-      -- the elements after this will appear in the middle of the statusline
-      status.component.fill(),
-      -- add a component to display if the LSP is loading, disable showing running client names, and use no separator
-      status.component.lsp { lsp_client_names = false, surround = { separator = "none", color = "bg" } },
-      -- fill the rest of the statusline
-      -- the elements after this will appear on the right of the statusline
-      status.component.fill(),
-      -- add a component for the current diagnostics if it exists and use the right separator for the section
-      status.component.diagnostics { surround = { separator = "right" } },
-      -- add a component to display LSP clients, disable showing LSP progress, and use the right separator
-      status.component.lsp { lsp_progress = false, surround = { separator = "right" } },
-      -- NvChad has some nice icons to go along with information, so we can create a parent component to do this
-      -- all of the children of this table will be treated together as a single component
       {
-        -- define a simple component where the provider is just a folder icon
+        hl = { bg = "folder_icon_bg", fg = "file_info_bg" },
+
+        -- status.component.builder {
+        --   { provider = "" },
+        --   padding = { left = 1 },
+        --   hl = { bg = "fg" },
+        -- },
         status.component.builder {
-          -- astronvim.get_icon gets the user interface icon for a closed folder with a space after it
-          { provider = require("astronvim.utils").get_icon "FolderClosed" },
-          -- add padding after icon
-          padding = { right = 1 },
-          -- set the foreground color to be used for the icon
-          hl = { fg = "bg" },
-          -- use the right separator and define the background color
-          surround = { separator = "right", color = "folder_icon_bg" },
+          { provider = get_icon "FolderClosed" },
+          hl = { bg = "folder_icon_bg", fg = "bg" },
+          surround = { separator = "tab", color = { main = "folder_icon_bg", right = "folder_icon_bg" } },
         },
-        -- add a file information component and only show the current working directory name
         status.component.file_info {
-          -- we only want filename to be used and we can change the fname
-          -- function to get the current working directory name
-          filename = { fname = function(nr) return vim.fn.getcwd(nr) end, padding = { left = 1 } },
-          -- disable all other elements of the file_info component
-          file_icon = false,
+          filename = {
+            fname = function(nr) return vim.fn.getcwd(nr) end,
+            surround = { separator = "none" },
+          },
+          padding = { right = 1, left = 1 },
           file_modified = false,
           file_read_only = false,
-          -- use no separator for this part but define a background color
-          surround = { separator = "none", color = "file_info_bg", condition = false },
+          file_icon = false,
+          hl = { fg = "bg", bg = "folder_icon_bg" },
+          surround = {
+            separator = "left",
+            color = function() return { right = status.hl.mode_bg(), main = "folder_icon_bg" } end,
+          },
         },
       },
-      -- the final component of the NvChad statusline is the navigation section
-      -- this is very similar to the previous current working directory section with the icon
-      { -- make nav section with icon border
-        -- define a custom component with just a file icon
-        status.component.builder {
-          { provider = require("astronvim.utils").get_icon "ScrollText" },
-          -- add padding after icon
-          padding = { right = 1 },
-          -- set the icon foreground
-          hl = { fg = "bg" },
-          -- use the right separator and define the background color
-          -- as well as the color to the left of the separator
-          surround = { separator = "right", color = { main = "nav_icon_bg", left = "file_info_bg" } },
+      status.component.mode {
+        mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 0 } } },
+        padding = { right = 1 },
+        surround = {
+          separator = "left",
+          color = function() return { main = status.hl.mode_bg(), right = "bg" } end,
         },
-        -- add a navigation component and just display the percentage of progress in the file
-        status.component.nav {
-          -- add some padding for the percentage provider
-          percentage = { padding = { right = 1 } },
-          -- disable all other providers
-          ruler = false,
-          scrollbar = false,
-          -- use no separator and define the background color
-          surround = { separator = "none", color = "file_info_bg" },
+      },
+
+      status.component.lsp {
+        lsp_progress = false,
+        surround = { separator = "none" },
+        hl = { fg = "buffer_overflow_fg" },
+        lsp_client_names = {
+          icon = {
+            padding = { right = 1, left = 0 },
+          },
+        },
+      },
+      status.component.fill(),
+      status.component.lsp { lsp_client_names = false, surround = { separator = "none", color = "bg" } },
+      status.component.diagnostics(),
+      status.component.nav {
+        ruler = { padding = { left = 1, right = 1 } },
+        percentage = false,
+        scrollbar = false,
+        surround = { separator = "none" },
+        hl = { bg = "bg", fg = "fg" },
+      },
+      status.component.treesitter { padding = { right = 1, left = 1 } },
+      status.component.file_info {
+        filetype = { padding = { left = 1 } },
+        filename = false,
+        file_icon = false,
+        file_modified = false,
+        padding = { right = 1 },
+        surround = { separator = "tab", color = { main = "command", left = "bg" } },
+        hl = { bg = "command", fg = "bg" },
+      },
+      status.component.git_diff {
+        surround = { separator = "none" },
+        hl = { bg = "bg" },
+        padding = { right = 1 },
+      },
+      status.component.git_branch {
+        hl = { bg = "git_branch_fg", fg = "bg" },
+        git_branch = { icon = { kind = "GitBranch" }, padding = { left = 1, right = 1 } },
+        surround = {
+          separator = "top_left",
+          color = "git_branch_fg",
         },
       },
     }
 
+    opts.winbar = {
+      -- create custom winbar
+      -- store the current buffer number
+      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+      fallthrough = false, -- pick the correct winbar based on condition
+      -- inactive winbar
+      {
+        condition = function() return not status.condition.is_active() end,
+        -- show the path to the file relative to the working directory
+        status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+        -- add the file name and icon
+        status.component.file_info {
+          file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbarnc", true),
+          surround = false,
+          update = "BufEnter",
+        },
+      },
+      -- active winbar
+      {
+        -- show the path to the file relative to the working directory
+        status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+        -- add the file name and icon
+        status.component.file_info { -- add file_info to breadcrumbs
+          file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbar", true),
+          surround = false,
+          update = "BufEnter",
+        },
+        -- show the breadcrumbs
+        status.component.breadcrumbs {
+          icon = { hl = true },
+          hl = status.hl.get_attributes("winbar", true),
+          prefix = true,
+          padding = { left = 0 },
+        },
+      },
+    }
     -- return the final options table
     return opts
   end,
