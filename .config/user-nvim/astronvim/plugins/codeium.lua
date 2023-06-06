13aa56a13e35f6e6306198ade2103587cccde7b9
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-06-06 15:55:15 +0100

    :recycle: Move codeium config to one place

--- .config/user-nvim/astronvim/plugins/codeium.lua
+++ .config/user-nvim/astronvim/plugins/codeium.lua
@@ -1,9 +1,26 @@
 return {
-  "jcdickinson/codeium.nvim",
-  event = "BufEnter",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
+  {
+    "jcdickinson/codeium.nvim",
+    event = "BufEnter",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function() require("codeium").setup {} end,
   },
-  config = function() require("codeium").setup {} end,
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "jcdickinson/codeium.nvim",
+    },
+    opts = function(_, opts)
+      local cmp = require "cmp"
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "codeium", priority = 2500 },
+      }))
+
+      return opts
+    end,
+  },
 }
