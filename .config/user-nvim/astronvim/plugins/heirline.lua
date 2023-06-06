c140576c33560e09d4e7ec8385ad60cdae3ac706
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-06-06 16:48:45 +0100

    :lipstick: More heirline configuration

--- .config/user-nvim/astronvim/plugins/heirline.lua
+++ .config/user-nvim/astronvim/plugins/heirline.lua
@@ -9,9 +9,16 @@ return {
       {
         hl = { bg = "folder_icon_bg", fg = "file_info_bg" },
         status.component.builder {
+          { provider = "" },
+          hl = { bg = "folder_icon_bg", fg = "bg" },
+          surround = { separator = "none" },
+          padding = { left = 1, right = 1 },
+        },
+        status.component.builder {
           { provider = get_icon "FolderClosed" },
           hl = { bg = "folder_icon_bg", fg = "bg" },
-          surround = { separator = "tab", color = { main = "folder_icon_bg", right = "folder_icon_bg" } },
+          surround = { separator = "none" },
+          padding = { left = 1, right = 1 },
         },
         status.component.file_info {
           filename = {
@@ -24,51 +31,60 @@ return {
           file_icon = false,
           hl = { fg = "bg", bg = "folder_icon_bg" },
           surround = {
-            separator = "left",
+            separator = "right",
             color = function() return { right = status.hl.mode_bg(), main = "folder_icon_bg" } end,
           },
         },
       },
       status.component.mode {
-        mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 0 } } },
+        mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 1 } } },
         padding = { right = 1 },
+        hl = function() return { bg = status.hl.mode_bg(), fg = "bg", bold = true } end,
         surround = {
-          separator = "left",
+          separator = "right",
           color = function() return { main = status.hl.mode_bg(), right = "bg" } end,
         },
       },
-
       status.component.lsp {
         lsp_progress = false,
         surround = { separator = "none" },
-        hl = { fg = "buffer_overflow_fg" },
+        hl = { fg = "buffer_overflow_fg", bg = "bg" },
+        padding = { right = 1 },
         lsp_client_names = {
+          truncate = 0.10,
           icon = {
-            padding = { right = 1, left = 0 },
+            padding = { right = 1, left = 1 },
           },
         },
       },
       status.component.fill(),
       status.component.lsp { lsp_client_names = false, surround = { separator = "none" } },
-      status.component.diagnostics { surround = { separator = "none" } },
-      status.component.nav {
-        ruler = { padding = { left = 1, right = 1 } },
-        percentage = false,
-        scrollbar = false,
+      status.component.fill(),
+      status.component.diagnostics {
         surround = { separator = "none" },
+        padding = { right = 1, left = 1 },
       },
-      status.component.treesitter { padding = { right = 1, left = 1 } },
+      -- filename
+      status.component.file_info {
+        padding = { right = 1 },
+        file_icon = false,
+        file_modified = false,
+        filename = { padding = { left = 1 } },
+        hl = { bg = "file_info_bg", fg = "white" },
+        surround = { separator = "left", color = { main = "file_info_bg", left = "bg" } },
+      },
+      -- filetype
       status.component.file_info {
         filetype = {},
         filename = false,
         file_icon = { padding = { left = 1 } },
         file_modified = false,
         padding = { right = 1 },
-        surround = { separator = "tab", color = { main = "command", left = "bg" } },
+        surround = { separator = "left", color = { main = "command", left = "file_info_bg" } },
         hl = { bg = "command", fg = "bg", bold = true },
       },
       status.component.git_diff {
-        surround = { separator = "none" },
+        surround = { separator = "left", color = { main = "bg", left = "command" } },
         hl = { bg = "bg" },
         padding = { right = 1 },
       },
@@ -76,9 +92,16 @@ return {
         hl = { bg = "git_branch_fg", fg = "bg" },
         git_branch = { icon = { kind = "GitBranch" }, padding = { left = 1, right = 1 } },
         surround = {
-          separator = "top_left",
-          color = "git_branch_fg",
+          separator = "left",
+          color = { main = "git_branch_fg", left = "bg" },
         },
+      },
+      status.component.nav {
+        percentage = false,
+        scrollbar = false,
+        hl = { bg = "bg", fg = "fg" },
+        ruler = { padding = { left = 1 } },
+        surround = { separator = "left", color = { main = "bg", left = "git_branch_fg" } },
       },
     }
 
