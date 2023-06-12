7d338b4a147a661408561f9ff2844a1c28e341ce
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2023-06-12 12:27:25 +0100

    :wrench: Disable saved notifications in noice

--- .config/user-nvim/astronvim/lsp/formatting.lua
+++ .config/user-nvim/astronvim/lsp/formatting.lua
@@ -1,7 +1,7 @@
 return {
   -- control auto formatting on save
   format_on_save = {
-    enabled = true, -- enable or disable format on save globally
+    enabled = true,     -- enable or disable format on save globally
     allow_filetypes = { -- enable format on save for specified filetypes only
       -- "go",
     },
@@ -13,7 +13,7 @@ return {
     -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
     -- "lua_ls",
   },
-  timeout_ms = 1000, -- default format timeout
+  timeout_ms = 2000, -- default format timeout
   -- filter = function(client) -- fully override the default formatting function
   --   return true
   -- end
