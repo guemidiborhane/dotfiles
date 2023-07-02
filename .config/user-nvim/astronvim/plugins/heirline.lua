ae43f00c43138eb0a10616c9d2eb25605b35e22b
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-07-02 09:20:18 +0100

    :bug: Fix heirline requires

--- .config/user-nvim/astronvim/plugins/heirline.lua
+++ .config/user-nvim/astronvim/plugins/heirline.lua
@@ -1,8 +1,8 @@
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
-    opts.statusline = require "plugins.configs.heirline.statusline"
-    opts.winbar = require "plugins.configs.heirline.winbar"
+    opts.statusline = require "user.plugins.configs.heirline.statusline"
+    opts.winbar = require "user.plugins.configs.heirline.winbar"
 
     return opts
   end,
