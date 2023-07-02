6c8bdb387f111d52710239b3d4a9ccc20a92581c
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-07-02 08:47:36 +0100

    :recycle: Extract heirline statusline and winbar to separate files

---
+++ .config/user-nvim/astronvim/plugins/disabled.lua
@@ -0,0 +1,6 @@
+-- Disabled plugins
+return {
+  { "rcarriga/nvim-dap-ui", enabled = false },
+  { "AstroNvim/astrotheme", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+}
