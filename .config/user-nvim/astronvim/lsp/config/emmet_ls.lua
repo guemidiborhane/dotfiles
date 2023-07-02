136f4290e147131b0a39b996e4b03b2e10804df6
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-07-02 08:54:07 +0100

    :truck: Move Emmet config to the correct location

---
+++ .config/user-nvim/astronvim/lsp/config/emmet_ls.lua
@@ -0,0 +1,28 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+return {
+  capabilities = capabilities,
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "typescriptreact",
+    "vue",
+  },
+  init_options = {
+    html = {
+      options = {
+        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+        ["bem.enabled"] = true,
+      },
+    },
+  },
+}
