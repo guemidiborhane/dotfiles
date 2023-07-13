798eb68bc1877bf35091c9adac314e07cf7abc6a
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-07-13 19:17:31 +0100

    :sparkles: Add crystal lang

--- .config/user-nvim/astronvim/plugins/mason.lua
+++ .config/user-nvim/astronvim/plugins/mason.lua
@@ -11,7 +11,10 @@ return {
         "ruby_ls",
         "emmet_ls",
         "yamlls",
+        "crystalline",
       })
+
+      return opts
     end,
   },
 
@@ -28,18 +31,11 @@ return {
         "prettierd",
         "htmlbeautifier",
         "yamllint",
+        "crystal_format",
       })
-    end,
-  },
+      opts.automatic_installation = true
 
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
-      })
+      return opts
     end,
   },
 }
