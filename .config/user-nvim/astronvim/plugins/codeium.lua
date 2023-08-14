160db0c18f89ab39ee28260d4b5aac77f75def29
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2023-08-14 21:20:11 +0100

    :arrow_up: Update codeium package

--- .config/user-nvim/astronvim/plugins/codeium.lua
+++ .config/user-nvim/astronvim/plugins/codeium.lua
@@ -2,7 +2,6 @@ return {
   {
     "jcdickinson/codeium.nvim",
     event = "User AstroFile",
-    commit = "b1ff0d6c993e3d87a4362d2ccd6c660f7444599f",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "hrsh7th/nvim-cmp",
