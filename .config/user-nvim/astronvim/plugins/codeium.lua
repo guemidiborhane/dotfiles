12ed048ec8cd6dc66456152adf82df5fbdff6061
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-08-17 16:12:55 +0100

    :bug: Temp Fix for codeium

--- .config/user-nvim/astronvim/plugins/codeium.lua
+++ .config/user-nvim/astronvim/plugins/codeium.lua
@@ -1,16 +1,7 @@
 return {
   {
-    "jcdickinson/codeium.nvim",
-    event = "User AstroFile",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function() require("codeium").setup {} end,
-  },
-
-  {
     "hrsh7th/nvim-cmp",
+    commit = "6c84bc75c64f778e9f1dcb798ed41c7fcb93b639",
     dependencies = {
       "jcdickinson/codeium.nvim",
     },
@@ -22,5 +13,15 @@ return {
 
       return opts
     end,
+  },
+
+  {
+    "jcdickinson/codeium.nvim",
+    event = "User AstroFile",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function() require("codeium").setup {} end,
   },
 }
