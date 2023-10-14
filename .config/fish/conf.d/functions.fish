aa1b09e89f56a33601d2e4cfa71b0b4a9c6a1d62
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2023-10-14 12:01:01 +0100

    :sparkles: starship

---
+++ .config/fish/conf.d/functions.fish
@@ -0,0 +1,28 @@
+function pagespeed
+    curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nResponse Code:\t\t%{response_code}\nCookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null --connect-timeout 1 $1
+end
+
+function kickoff
+    if test -d $1
+        echo "Directory $1 already exists"
+        return 1
+    end
+
+    set git_repo "git@git.netsys.dz:guemidiborhane/kickoff.git"
+    git clone $git_repo $1
+    cd $1
+    bash install.sh
+    code .
+end
+
+function tn
+    if count $argv > 0
+        set session_name $argv[1]
+    else
+        set session_name (basename $PWD)
+    end
+    tmux new-session -As "$session_name"
+end
+
+bind \ct 'tn \n'
+
--- .config/yadm/bootstrap.d/utils.sh
+++ .config/yadm/bootstrap.d/utils.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 # vim: set ft=sh sw=4 et :
 
-ask () {
-    if [ "$INSTALL_ALL" = "yes" ]; then
-        return 0
-    fi
+ask() {
+	if [ "$INSTALL_ALL" = "yes" ]; then
+		return 0
+	fi
 
-    valid_answers="y/N"
+	valid_answers="y/N"
 
-    if [ ! -z "$2" ]; then
-        valid_answers="$2"
-    fi
+	if [ ! -z "$2" ]; then
+		valid_answers="$2"
+	fi
 
-    read -p "$1? [$valid_answers] " -n 1 -r
-    echo   # (optional) move to a new line
+	read -p "$1? [$valid_answers] " -n 1 -r
+	echo # (optional) move to a new line
 }
 
-section () {
-    echo
-    echo "===================="
-    echo " $1"
-    echo "===================="
-    echo
+section() {
+	echo
+	echo "===================="
+	echo " $1"
+	echo "===================="
+	echo
 }
 
-readme () {
-    section "README"
-    echo "- Don't forget to run \`chsh -s \$(which zsh)\` to set zsh as your default shell"
-    echo "- be sure to login to your Mega account using \`mega-login <email> <password>\`"
-    echo "- You should probably reboot now ! just to be sure"
+readme() {
+	section "README"
+	echo "- Don't forget to run \`chsh -s \$(which fish)\` to set your default shell"
+	echo "- be sure to login to your Mega account using \`mega-login <email> <password>\`"
+	echo "- You should probably reboot now ! just to be sure"
 }
