015b7782361203e9596b6f9ed1b1f8f198bfdae4
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2023-11-09 19:47:03 +0100

    :bug: Fix pagespeed function for fish

--- .config/fish/conf.d/functions.fish
+++ .config/fish/conf.d/functions.fish
@@ -1,5 +1,5 @@
 function pagespeed
-    curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nResponse Code:\t\t%{response_code}\nCookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null --connect-timeout 1 $1
+    curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nResponse Code:\t\t%{response_code}\nCookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null --connect-timeout 1 $argv[1]
 end
 
 function kickoff
