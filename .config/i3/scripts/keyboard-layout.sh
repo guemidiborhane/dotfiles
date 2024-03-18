155bae53ddf6f9faa1e1e61048abf6fedd0ba423
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2024-03-18 01:54:24 +0100

    :bug: Set mouse natural scroll

--- .config/i3/scripts/keyboard-layout.sh
+++ .config/i3/scripts/keyboard-layout.sh
@@ -2,7 +2,7 @@
 input-remapper-control --command stop-all && input-remapper-control --command autoload
 notify-send "input remapper autoloaded"
 
-sleep 2
+sleep 1
 
 setxkbmap -layout fr -variant us-azerty
 notify-send "Keyboard layout set"
