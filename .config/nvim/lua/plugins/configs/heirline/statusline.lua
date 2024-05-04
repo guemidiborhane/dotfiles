207369e6c964cec0e42c6619bf84b1cda0230cec
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-05-05 00:42:50 +0100

    :lipstick: No fancy separators needed

--- .config/nvim/lua/plugins/configs/heirline/statusline.lua
+++ .config/nvim/lua/plugins/configs/heirline/statusline.lua
@@ -6,38 +6,22 @@ return {
     mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 1 } } },
     padding = { right = 1 },
     hl = function() return { bg = status.hl.mode_bg(), fg = "bg", bold = true } end,
-    surround = {
-      separator = "none",
-    },
-  },
-  status.component.builder {
-    { provider = "" },
-    surround = {
-      separator = "right",
-      color = function() return { main = status.hl.mode_bg(), right = "command" } end,
-    },
   },
   -- filetype
   status.component.file_info {
     filetype = {},
     filename = false,
-    file_icon = { padding = { left = 0 } },
+    file_icon = { padding = { left = 1 } },
     file_modified = false,
     padding = { right = 1 },
     surround = {
-      separator = "left_inner",
-      -- color = function() return { main = status.hl.mode_bg(), left = "command" } end,
       color = function() return { main = "command", left = "command" } end,
     },
     hl = { bg = "command", fg = "bg", bold = true },
   },
-  status.component.builder {
-    { provider = "" },
-    surround = { separator = "right", color = "command" },
-  },
   status.component.lsp {
     lsp_progress = false,
-    surround = { separator = "none" },
+    surround = {},
     hl = { fg = "muted_fg", bg = "bg" },
     padding = { right = 0 },
     lsp_client_names = {
@@ -54,34 +38,29 @@ return {
         provider = get_icon "ScrollText",
       },
       hl = { bg = "bg", fg = "muted_fg" },
-      surround = { separator = "none" },
       padding = { right = 1 },
     },
     status.component.nav {
       percentage = false,
       hl = { bg = "bg", fg = "muted_fg" },
       scrollbar = false,
-      ruler = { padding = { right = 1 }, surround = { separator = "none" } },
-      surround = { separator = "none", condition = false },
+      ruler = { padding = { right = 1 }, surround = {} },
+      surround = { condition = false },
     },
   },
   status.component.diagnostics {
-    surround = { separator = "none" },
+    surround = {},
     hl = { bg = "bg" },
-    padding = { right = 0, left = 0 },
-  },
-  status.component.builder {
-    { provider = "" },
-    surround = { separator = "left", color = "bg" },
+    padding = { right = 1, left = 0 },
   },
   status.component.git_diff {
-    surround = { separator = "none" },
+    surround = {},
     hl = { bg = "bg", fg = "command" },
     padding = { right = 1 },
   },
   status.component.git_branch {
-    hl = { bg = "git_branch_fg", fg = "bg" },
+    hl = { fg = "bg" },
     git_branch = { icon = { kind = "GitBranch" }, padding = { left = 1, right = 1 } },
-    surround = { separator = "left", color = "git_branch_fg" },
+    surround = { separator = "none", color = "git_branch_fg" },
   },
 }
