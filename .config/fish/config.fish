ca4c6027413f43605e0c3c7b2c6f4b38a53349ef
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-05-11 08:31:03 +0100

    :truck: Migrate to sesh for tmux session management

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,8 +15,6 @@ set -gx MCFLY_RESULTS_SORT "LAST_RUN"
 set -gx MCFLY_RESULTS 100
 mcfly init fish | source
 
-set -gx PATH "$PATH:$HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin"
-
 source /opt/asdf-vm/asdf.fish
 set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
 . ~/.asdf/plugins/golang/set-env.fish
