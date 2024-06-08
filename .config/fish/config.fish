1bfb081c2d3752b8dcec3176ad76928d4949304f
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-06-09 00:17:17 +0100

    :wrench: Enable transient prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,8 +17,6 @@ source /opt/asdf-vm/asdf.fish
 set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
 . ~/.asdf/plugins/golang/set-env.fish
 
-starship init fish | source
-
 zoxide init --cmd cd fish | source
 fzf --fish | source
 
@@ -31,4 +29,15 @@ if status --is-login
 
     figlet -w $(tput cols) -f "ANSI Shadow.flf" 'I Rock, you suck. Stinson out!' | lolcat
 end
+
+function starship_transient_prompt_func
+  starship module character
+end
+
+function starship_transient_rprompt_func
+  starship module time
+end
+
+starship init fish | source
+enable_transience
 
