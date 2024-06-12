77994c3e821573aacc3e8c01100204b2a203196b
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-06-12 12:51:36 +0100

    :broom: Cleanup i3 conf dir

--- .config/i3/scripts/keyboard-layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-input-remapper-control --command stop-all && input-remapper-control --command autoload
-notify-send "input remapper autoloaded"
-
-sleep 1
-
-setxkbmap -layout fr -variant us-azerty
-notify-send "Keyboard layout set"
--- .config/i3/scripts/mouse-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# vim: set ft=sh sw=4 et :
-
-solaar config 1 dpi 800
-solaar config 1 hires-smooth-invert true
-xinput set-prop "pointer:Logitech MX Master 3" "libinput Accel Speed" -0.5
-xinput set-prop "pointer:Logitech MX Master 3" 'libinput Accel Profile Enabled' 0, 1
-notify-send 'Configured Logitech MX Master 3'
