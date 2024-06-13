746d412ad84a0189facc5c3b7401f4a14b92ff35
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-06-13 19:30:36 +0100

    :broom: general cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,6 +29,7 @@ set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
 . ~/.asdf/plugins/golang/set-env.fish
 
 zoxide init --cmd cd fish | source
+set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
 fzf --fish | source
 source /usr/share/doc/find-the-command/ftc.fish quiet
 fish_ssh_agent
