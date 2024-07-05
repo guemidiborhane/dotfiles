3041ab353d947a322f6a112f4c7f6bc7596834ea
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2024-07-05 06:55:29 +0100

    :wrench: fish: move env variables to .profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 # vi: set ft=fish :
 
-fish_vi_key_bindings
 function fish_greeting
     fastfetch
 end
@@ -13,23 +12,7 @@ function starship_transient_rprompt_func
   starship module time
 end
 
-set -Ux PAGER less
-set -Ux EDITOR nvim
-set -Ux VISUAL nvim
-
-set -gx ANSIBLE_NOCOWS true
-
-set -gx PATH "$PATH:$HOME/.local/bin"
-set -gx PATH "$PATH:$HOME/.krew/bin"
-
-set -gx BAT_THEME "Dracula"
-
-source /opt/asdf-vm/asdf.fish
-set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
-. ~/.asdf/plugins/golang/set-env.fish
-
 zoxide init --cmd cd fish | source
-set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
 fzf --fish | source
 source /usr/share/doc/find-the-command/ftc.fish quiet
 fish_ssh_agent
@@ -41,7 +24,6 @@ if status is-login
 end
 
 starship init fish | source
+fish_vi_key_bindings
 enable_transience
 
-# Required for touchscreen gestures in firefox
-set -gx MOZ_USE_XINPUT2 1
--- .profile
+++ .profile
@@ -1,5 +1,16 @@
 export TERMINAL=wezterm
 export BROWSER=/usr/bin/firefox
 
-# Added by Toolbox App
 export PATH="$PATH:$HOME/.asdf/shims"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.krew/bin"
+
+export PAGER=less
+export EDITOR=nvim
+export VISUAL=nvim
+export ANSIBLE_NOCOWS=true
+export BAT_THEME="Dracula"
+export FZF_DEFAULT_OPTS="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
+# Required for touchscreen gestures in firefox
+export MOZ_USE_XINPUT2=true
+export QT_QPA_PLATFORMTHEME=qt5ct
