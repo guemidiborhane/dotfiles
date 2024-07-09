d2ac99c59ad96febb322a224d165449699ebee05
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2024-07-09 06:01:23 +0100

    :wrench: polybar: update cpu frequency module script

--- .config/polybar/scripts/cpu-frequency.sh
+++ .config/polybar/scripts/cpu-frequency.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
 
-c=0
-t=0
-
-awk '/MHz/ {print $4}' </proc/cpuinfo | (
-	while read -r i; do
-		t=$(echo "$t + $i" | bc)
-		c=$((c + 1))
-	done
-
-	avg_frequency=$(echo "scale=2; $t / $c / 1000" | bc)
-	icon=""
-	if [ $(echo "$avg_frequency < 3.4" | bc) -eq 1 ]; then
-		icon="󰾆"
-	elif [ $(echo "$avg_frequency < 4" | bc) -eq 1 ]; then
-		icon="󰾅"
-	else
-		icon="󰓅"
-	fi
-
-	echo "$icon $avg_frequency GHz"
-)
+~/.local/bin/core_frequencies | sort -k3 -r | head -1 | awk '{print $3 " " $4}' | xargs -I {} echo "󰾅 {}"
