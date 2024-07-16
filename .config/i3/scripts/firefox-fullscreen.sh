d2779050eb817d3f283bfa12138ac6bca1c30204
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-07-16 19:48:23 +0100

    :fire: no more needed

--- .config/i3/scripts/firefox-fullscreen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Get active window properties for writing `for_window` rules
-# In ~/.i3/config:
-#    bindsym $mod2+f ~/.config/i3/scripts/firefox-fullscreen.sh
-
-i3-msg fullscreen toggle
-sleep 0.3
-xdotool key --window $(xdotool getactivewindow) f
