23f749902f9411917ae6e6366490a2097f498516
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-07-22 18:00:14 +0100

    :bug: fish: don't startx on ssh sessions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,7 +18,7 @@ fzf --fish | source
 source /usr/share/doc/find-the-command/ftc.fish quiet
 
 if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1 -a -z "$SSH_CONNECTION" -a (tty | string match -r '/dev/tty[0-9]+')
         exec startx
     end
 end
