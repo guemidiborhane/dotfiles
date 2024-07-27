ff90454b9344d39291103aa813e2f5fce8d518d1
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-07-27 21:09:09 +0100

    :fire: yadm: remove mega install script

--- .config/yadm/bootstrap.d/utils.sh
+++ .config/yadm/bootstrap.d/utils.sh
@@ -2,31 +2,30 @@
 # vim: set ft=sh sw=4 et :
 
 ask() {
-	if [ "$INSTALL_ALL" = "yes" ]; then
-		return 0
-	fi
+    if [ "$INSTALL_ALL" = "yes" ]; then
+        return 0
+    fi
 
-	valid_answers="y/N"
+    valid_answers="y/N"
 
-	if [ ! -z "$2" ]; then
-		valid_answers="$2"
-	fi
+    if [ ! -z "$2" ]; then
+        valid_answers="$2"
+    fi
 
-	read -p "$1? [$valid_answers] " -n 1 -r
-	echo # (optional) move to a new line
+    read -p "$1? [$valid_answers] " -n 1 -r
+    echo # (optional) move to a new line
 }
 
 section() {
-	echo
-	echo "===================="
-	echo " $1"
-	echo "===================="
-	echo
+    echo
+    echo "===================="
+    echo " $1"
+    echo "===================="
+    echo
 }
 
 readme() {
-	section "README"
-	echo "- Don't forget to run \`chsh -s \$(which fish)\` to set your default shell"
-	echo "- be sure to login to your Mega account using \`mega-login <email> <password>\`"
-	echo "- You should probably reboot now ! just to be sure"
+    section "README"
+    echo "- Don't forget to run \`chsh -s \$(which fish)\` to set your default shell"
+    echo "- You should probably reboot now ! just to be sure"
 }
