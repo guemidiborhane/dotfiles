8bd7b5651149db8669a45bf730fcefb76706e293
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-08-01 20:11:20 +0100

    :recycle: tmux: only attempt to kill panes running $SHELL

--- .config/tmux/scripts/kill_pane.sh
+++ .config/tmux/scripts/kill_pane.sh
@@ -4,16 +4,13 @@ main() {
 	panes_count=$(tmux list-panes -s | wc -l)
 	default_shell=$(basename "$SHELL")
 	current_shell=$(tmux display-message -p '#{pane_current_command}')
-
-	pane_pid="$(tmux display -p "#{pane_pid}")"
-
-	if [ "$panes_count" -gt 1 ]; then
-		tmux send-keys C-d
-	else
-		tmux display-popup -w 25 -h 7 -E gum confirm "kill-pane ?" --default=0 --timeout=3s --no-show-help
-
-		if [ $? -eq 0 ]; then
-			 tmux send-keys C-d
+	if [ "$default_shell" = "$current_shell" ]; then
+		if [ "$panes_count" -gt 1 ]; then
+			tmux send-keys C-d
+		else
+			if tmux popup -w 25 -h 6 -E gum confirm "kill-pane ?" --default=1 --no-show-help; then
+				tmux send-keys C-d
+			fi
 		fi
 	fi
 
