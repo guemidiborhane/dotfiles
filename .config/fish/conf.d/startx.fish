384f0ef99e292a21fbc0511e487861a6362eb30c
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2024-11-05 11:33:31 +0100

    :sparkles: session: add window manager selector

--- .config/fish/conf.d/startx.fish
+++ .config/fish/conf.d/startx.fish
@@ -1,6 +1,9 @@
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1 -a -z "$SSH_CONNECTION" -a (tty | string match -r '/dev/tty[0-9]+')
-        exec ~/.config/hypr/hyprstart
+        set choice (start_session)
+        if test -n "$choice"
+            exec sh -c "$choice"
+        end
     end
 end
 
