cf55226f78d3fb244c821052a8247c2d04e89cc6
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-11-29 02:56:47 +0100

    :wrench: wezterm: tabs are just meant to be at the top

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.check_for_updates = false
 
 config.color_scheme = "Dracula (Official)"
 config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.window_decorations = "NONE"
 
