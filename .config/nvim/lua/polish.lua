0a50c85e0e2e2919f43e2412f48966dc07d77497
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2024-11-29 02:52:34 +0100

    :wrench: nvim: disable the evil arrow keys

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,7 +1,17 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+-- Show an error message if a disabled key is pressed
+local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
 
--- Set up custom filetypes
+-- Disable arrow keys in insert mode with a styled message
+vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
+
+-- Disable arrow keys in normal mode with a styled message
+vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
