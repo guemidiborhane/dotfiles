5964877b10a249de8dabe8a167e55ff7743139e5
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2025-02-25 15:13:12 +0100

    :wrench: tweaks

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,6 +7,14 @@ local map = require("utils").set_keymap
 map("i", "jj", "<Esc>")
 
 -- Buffers
-map("n", "<Tab>", ":bnext<CR>", "Next open Buffer on tabline")
-map("n", "<S-Tab>", ":bprevious<CR>", "Previous open Buffer on tabline")
-map("n", "<leader>bf", ":Telescope buffers<CR>", "Find Buffer")
+map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", "Next open Buffer on tabline")
+map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", "Previous open Buffer on tabline")
+map("n", "<leader>bf", "<cmd>Telescope buffers<CR>", "Find Buffer")
+
+local show_dashboard = function()
+  vim.cmd("enew")
+  Snacks.dashboard()
+end
+-- stylua: ignore start
+map("n", "<leader>h", show_dashboard, "Show dashboard")
+-- stylua: ignore end
--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,21 +1,21 @@
 return {
   "snacks.nvim",
-  config = function(_, opts)
-    require("snacks").setup(opts)
-
-    vim.api.nvim_create_autocmd("BufDelete", {
-      group = vim.api.nvim_create_augroup("dashboard_on_close", { clear = true }),
-      callback = function(args)
-        local deleted_name = vim.api.nvim_buf_get_name(args.buf)
-        local deleted_ft = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
-        local dashboard_on_empty = (deleted_name == "" and deleted_ft == "")
-          or (vim.api.nvim_buf_get_name(0) == "" and vim.api.nvim_get_option_value("filetype", { buf = 0 }) == "")
-        if dashboard_on_empty then
-          Snacks.dashboard()
-        end
-      end,
-    })
-  end,
+  -- config = function(_, opts)
+  --   require("snacks").setup(opts)
+  --
+  --   vim.api.nvim_create_autocmd("BufDelete", {
+  --     group = vim.api.nvim_create_augroup("dashboard_on_close", { clear = true }),
+  --     callback = function(args)
+  --       local deleted_name = vim.api.nvim_buf_get_name(args.buf)
+  --       local deleted_ft = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+  --       local dashboard_on_empty = (deleted_name == "" and deleted_ft == "")
+  --         or (vim.api.nvim_buf_get_name(0) == "" and vim.api.nvim_get_option_value("filetype", { buf = 0 }) == "")
+  --       if dashboard_on_empty then
+  --         Snacks.dashboard()
+  --       end
+  --     end,
+  --   })
+  -- end,
   opts = {
     dashboard = {
       preset = {
