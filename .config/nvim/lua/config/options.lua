44c3834a6e94b222ae72d3f72cb28cfa0e7a6b41
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2025-02-25 15:13:12 +0100

    :wrench: tweaks

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,11 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local o = vim.opt
 -- disable animations
 vim.g.snacks_animate = false
 
 -- disable swapfiles
-vim.opt.swapfile = false
+o.swapfile = false
 
 -- sync buffers between neovim windows
-vim.opt.autoread = true
+o.autoread = true
+
+-- faster completion
+o.updatetime = 250
