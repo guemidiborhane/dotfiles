8c6389a0e8138f517c2e1ab35345a490db17042e
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-05-21 03:50:13 +0100

    :wrench: tmux: minimal status bar

---
+++ .config/tmux/scripts/path-widget.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+current_path="${1}"
+default_path_format="relative"
+PATH_FORMAT="${PATH_FORMAT:-$default_path_format}"
+
+# Check user requested format
+if [[ ${PATH_FORMAT} == "relative" ]]; then
+  current_path="$(echo ${current_path} | sed 's#'"$HOME"'#~#g')"
+fi
+
+# Determine if current path is in a git repository
+if git -C "${1}" rev-parse --is-inside-work-tree &>/dev/null; then
+  # Git repository detected, use git branch icon
+  icon=""
+else
+  # Not a git repository, use folder icon
+  icon=""
+fi
+
+echo "#[fg=black,bg=#BD93F9] ${icon} ${current_path} "
