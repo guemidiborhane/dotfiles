73bc8d006a4aaea68c366e9a0b36bb0ce592e389
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-06-19 18:30:33 +0100

    :recycle: tmux: handle errors more gracefully in make subcommand

--- .config/tmux/scripts/make-fzf.sh
+++ .config/tmux/scripts/make-fzf.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+# Exit if no Makefile present
+[ ! -f Makefile ] && exit 0
+
 task="$(make -npq | awk -v RS= -F: '$1 ~ /^[^#%.]+$/ { print $1 }' | fzf-tmux -p)"
+
+# Exit if task is empty/cancelled
+[ -z "$task" ] && exit 0
+
 breakout_type="$(echo -e 'window\nhsplit\nvsplit' | fzf-tmux -p)"
+
+# Exit if breakout_type is empty/cancelled
+[ -z "$breakout_type" ] && exit 0
+
 tmux_command=""
 
 case "$breakout_type" in
