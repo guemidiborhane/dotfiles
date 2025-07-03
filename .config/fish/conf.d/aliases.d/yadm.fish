895d0e48b833eb32d985d19e842bae406e39ade4
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-07-03 16:08:46 +0100

    :wrench: fish/tmux: add script to kill sessions

--- .config/fish/conf.d/aliases.d/yadm.fish
+++ .config/fish/conf.d/aliases.d/yadm.fish
@@ -2,6 +2,6 @@
 abbr ye "yadm edit"
 abbr yu "yadm pull --rebase"
 abbr yp "yadm push"
-abbr gg "tmux_popup yadm enter lazygit"
+abbr gg "tmux_popup -w 90% -h 90% yadm enter lazygit"
 abbr yn "yadm enter nvim"
 abbr ys "yadm st"
