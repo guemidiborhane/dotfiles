22c511f6586648be5183344e97665b4c09580270
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-07-03 15:29:31 +0100

    :wrench: nvim: fix telescope find git_files
    show untracked files by default and don't show when editing dotfiles
    (YADM)

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,11 @@ return {
     {
       "<leader><space>",
       function()
-        local ok = pcall(require("telescope.builtin").git_files)
+        local git_dir = os.getenv("GIT_DIR")
+        local in_yadm = git_dir and git_dir:match("yadm/repo%.git$") ~= nil
+        local show_untracked = not in_yadm
+
+        local ok = pcall(require("telescope.builtin").git_files, { show_untracked = show_untracked })
         if not ok then
           require("telescope.builtin").find_files()
         end
