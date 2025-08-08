cdc5aa73e84e5f0396ba4e6d76bd75c6a36361c3
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-08-08 15:17:50 +0100

    :bug: nvim: fix rubocop

--- .config/nvim/lua/plugins/ruby_ls.lua
+++ .config/nvim/lua/plugins/ruby_ls.lua
@@ -7,6 +7,20 @@ return {
           mason = false,
           cmd = { vim.fn.expand("~/.local/share/mise/shims/ruby-lsp") },
         },
+        rubocop = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.local/share/mise/shims/rubocop") },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        rubocop = {
+          command = "~/.local/share/mise/shims/rubocop",
+        },
       },
     },
   },
