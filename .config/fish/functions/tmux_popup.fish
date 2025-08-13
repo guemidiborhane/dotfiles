873676ebc14b7ac7cbf9a7c109ba0056fb43d911
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-08-13 23:53:02 +0100

    :bug: fish: fix tmux popup helper not handling options correctly

--- .config/fish/functions/tmux_popup.fish
+++ .config/fish/functions/tmux_popup.fish
@@ -6,7 +6,28 @@ function tmux_popup -d "Run command in tmux popup or directly if not in tmux"
     end
 
     if test -z "$TMUX"
-        $argv
+        # Filter out tmux-specific options when not in tmux
+        set -l filtered_args
+        set -l skip_next false
+
+        for arg in $argv
+            if test "$skip_next" = true
+                set skip_next false
+                continue
+            end
+
+            switch $arg
+                case -w -h -x -y -S -t -d
+                    set skip_next true
+                case '-*'
+                    # Skip other tmux popup flags
+                    continue
+                case '*'
+                    set -a filtered_args $arg
+            end
+        end
+
+        $filtered_args
     else
         tmux popup -d '#{pane_current_path}' -E $argv
     end
