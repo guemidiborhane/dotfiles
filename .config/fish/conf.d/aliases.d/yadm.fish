77347f4e99fd879e7eeb62d67fe857c64349e760
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-08-13 23:53:02 +0100

    :bug: fish: fix tmux popup helper not handling options correctly

--- .config/fish/conf.d/aliases.d/yadm.fish
+++ .config/fish/conf.d/aliases.d/yadm.fish
@@ -1,7 +1,8 @@
-# vim ft=fish
 abbr ye "yadm edit"
 abbr yu "yadm pull --rebase"
 abbr yp "yadm push"
 abbr gg "tmux_popup -w 90% -h 90% yadm enter lazygit"
 abbr yn "yadm enter nvim"
 abbr ys "yadm st"
+
+# vim ft=fish
