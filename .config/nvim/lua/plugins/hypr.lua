e6de860001fff2fae44f1105bb3dc05973f66ba8
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-09-25 12:35:33 +0100

    :arrow_up: nvim: update to lazyvim 15

--- .config/nvim/lua/plugins/hypr.lua
+++ .config/nvim/lua/plugins/hypr.lua
@@ -4,7 +4,7 @@ return {
     opts = { ensure_installed = { "hyprlang" } },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "hyprls",
