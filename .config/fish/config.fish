guemidiborhane/dotfiles

commit f2c8a461165914aa704acaa31dd671d337225e3a

:wrench: fish: move functions to separate files and add basic aliases
Credit: https://github.com/folke/dot

.config/fish/config.fish
@@ -1,36 +1,26 @@
 # vi: set ft=fish :
+status is-interactive; or exit
 
-function fish_greeting
-    fastfetch
-end
+set -gx EDITOR (which nvim)
+set -gx VISUAL $EDITOR
+set -gx SUDO_EDITOR $EDITOR
 
-function starship_transient_prompt_func
-    starship module character
-end
-
-function starship_transient_rprompt_func
-    starship module directory
-    starship module time
-end
-
-set -Ux EDITOR nvim
-set -Ux VISUAL nvim
+set -x LESS -rF
 set -Ux PAGER less -R
 set -Ux MANPAGER nvim +Man!
+
 set -Ux ANSIBLE_NOCOWS true
 set -Ux BAT_THEME Dracula
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
 
-if status is-interactive
-    zoxide init --cmd cd fish | source
-    starship init fish | source
-    set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
-    carapace _carapace | source
-    mise activate fish | source
-    atuin init fish --disable-up-arrow | source
-    dr --completion fish | source
-    source /usr/share/doc/pkgfile/command-not-found.fish
+zoxide init --cmd cd fish | source
+starship init fish | source
+set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
+carapace _carapace | source
+mise activate fish | source
+atuin init fish --disable-up-arrow | source
+dr --completion fish | source
+source /usr/share/doc/pkgfile/command-not-found.fish
 
-    fish_vi_key_bindings
-    enable_transience
-end
+fish_vi_key_bindings
+enable_transience

.config/fish/conf.d/aliases.fish
@@ -1,4 +1,9 @@
 # vi: set ft=fish :
+status is-interactive; or exit
+
+abbr mv "mv -iv"
+abbr cp "cp -riv"
+abbr mkdir "mkdir -vp"
 
 # source: https://denshub.com/en/best-ls-command-alternative/#first-time
 alias ls 'eza --color=always --icons=auto --group-directories-first'

.config/fish/conf.d/ssh.fish
@@ -1,3 +1,5 @@
+status is-interactive; or exit
+
 if test $status -eq 0
     and test -z "$TMUX"
     and test -n "$SSH_TTY"

.config/fish/functions/fish_greeting.fish
@@ -1,0 +1,5 @@
+function fish_greeting
+    fastfetch
+end
+
+# vim ft=fish

.config/fish/functions/starship_transient_prompt_func.fish
@@ -1,0 +1,5 @@
+function starship_transient_prompt_func
+    starship module character
+end
+
+# vim ft=fish

.config/fish/functions/starship_transient_rprompt_func.fish
@@ -1,0 +1,6 @@
+function starship_transient_rprompt_func
+    starship module directory
+    starship module time
+end
+
+# vim ft=fish

commit 54f9539e71edbb9da399983f24d431e8bfe1e6d0

:bug: fish: fix necessary variables not being set
Introduced in f2c8a461

.config/fish/config.fish
@@ -1,5 +1,4 @@
 # vi: set ft=fish :
-status is-interactive; or exit
 
 set -gx EDITOR (which nvim)
 set -gx VISUAL $EDITOR
@@ -12,6 +11,8 @@ set -Ux MANPAGER nvim +Man!
 set -Ux ANSIBLE_NOCOWS true
 set -Ux BAT_THEME Dracula
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
+
+status is-interactive; or exit
 
 zoxide init --cmd cd fish | source
 starship init fish | source

.config/hypr/conf.d/workspaces.conf
@@ -1,5 +1,5 @@
 # vim: ft=hyprlang
 
-workspace = special:$scratch, on-created-empty:$run -- $terminal -e fish -c "wn"
+workspace = special:$scratch, on-created-empty:$run -- $terminal -e fish -c "tn workshop"
 
 source = ~/.config/hypr/conf.d/workspace_monitor.conf