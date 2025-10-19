35022be44ffcc1e8b9a1362cc9752333d5aa365c
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-10-19 11:44:04 +0100

    :wrench: fish: consolidate aliases and abbreviations

--- .config/fish/conf.d/aliases.d/systemd.fish
+++
@@ -1,6 +0,0 @@
-abbr sc "sudo systemctl"
-abbr scu "systemctl --user"
-abbr jf "journalctl -fu"
-abbr jfu "journalctl --user -fu"
-
-# vim ft=fish
--- .config/fish/conf.d/aliases.d/yadm.fish
+++
@@ -1,7 +0,0 @@
-abbr ye "yadm edit"
-abbr yu "yadm pull --rebase"
-abbr yp "yadm push"
-abbr yn "yadm enter nvim"
-abbr ys "yadm st"
-
-# vim ft=fish
