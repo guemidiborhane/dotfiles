ffbe76846b4a0d92c4223c2e847a2366a45b064c
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-10-19 11:44:04 +0100

    :wrench: fish: consolidate aliases and abbreviations

--- .config/fish/conf.d/aliases.d/kubectl.fish
+++
@@ -1,5 +0,0 @@
-abbr --position anywhere k kubectl
-abbr --position anywhere ke kubectl edit
-abbr --position anywhere kg kubectl get
-
-# vim ft=fish
--- .config/fish/conf.d/aliases.d/make.fish
+++
@@ -1,7 +0,0 @@
-# vi: set ft=fish :
-set mk make
-abbr mk $mk
-abbr mkd "$mk dev"
-abbr mkdd "$mk down"
-abbr mkc "$mk console"
-abbr mkr "$mk restart"
