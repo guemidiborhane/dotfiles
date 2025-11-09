4db8bf158e1dc6996bf26669056e87676cd82d5f
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2025-11-09 07:26:14 +0100

    :lipstick: nvim: merge status line in tmux status

--- .config/nvim/lua/plugins/dracula.lua
+++ .config/nvim/lua/plugins/dracula.lua
@@ -1,12 +1,35 @@
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim" },
+  {
+    "Mofiqul/dracula.nvim",
+    opts = function(_, opts)
+      local dracula = require("dracula")
+      local colors = dracula.colors()
+
+      return {
+        lualine_bg_color = colors.bg,
+        overrides = {
+          StatusLine = { bg = colors.bg, fg = colors.fg },
+        },
+      }
+    end,
+  },
 
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "dracula",
+    },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "dracula-nvim",
+      },
     },
   },
 }
