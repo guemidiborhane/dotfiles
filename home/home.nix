f3b954d171cbf310405ba2683e52ff3a5272a2f5
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-01-08 00:18:56 +0100

    :recycle: home/pkgs: add user base packages

--- home/home.nix
+++ home/home.nix
@@ -7,6 +7,7 @@
   username = "${meta.username}";
   homeDirectory = "/home/${meta.username}";
 
+  packages = import ./pkgs.nix { inherit pkgs; };
 
   stateVersion = "25.11";
 }
---
+++ home/pkgs.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  ...
+}: with pkgs; [
+  tmux
+  waybar
+  bitwarden-desktop
+  wlogout
+  dracula-theme
+  kora-icon-theme
+  banana-cursor
+  hyprlock
+  wiremix
+  pulseaudio
+  brightnessctl
+  networkmanagerapplet
+  glib # gsettings
+]
