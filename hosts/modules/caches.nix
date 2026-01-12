95c73420cff4a46034e28c0208d84c379af1b6ce
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-01-12 12:31:34 +0100

    :arrow_up: nix: update

--- hosts/modules/caches.nix
+++ hosts/modules/caches.nix
@@ -8,4 +8,8 @@
     url = "https://attic.xuyh0120.win/lantian";
     key = "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=";
   }
+  {
+    url = "https://nltch.cachix.org";
+    key = "nltch.cachix.org-1:W85YxOt0XRZOP3Yppt+HNz3fXRu6DXgH3Ob9n9A+7Ec=";
+  }
 ]
