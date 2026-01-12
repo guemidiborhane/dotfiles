a9fe195d2e8a62148655093b48c028bc63b6438d
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-01-12 19:27:48 +0100

    :bug: home/programs: don't overwrite git config from dotfiles

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.git = {
     enable = true;
+    package = pkgs.unstable.git;
     lfs.enable = true;
   };
+  xdg.configFile."git/config".enable = lib.mkForce false;
 
   programs = {
     lazygit.enable = true;
