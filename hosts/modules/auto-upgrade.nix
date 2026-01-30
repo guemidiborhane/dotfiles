83a30a488f5d9caa84d69e16f32f1b38430cfbac
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-01-30 23:07:10 +0100

    :wrench: nix/config: update auto-upgrade to respect committed flake.lock
    - Rename auto_update/auto_reboot to autoUpdate/autoReboot for
    consistency
    - Remove --recreate-lock-file and --no-write-lock-file from auto-upgrade
      to respect the flake.lock for reproducibility

--- hosts/modules/auto-upgrade.nix
+++ hosts/modules/auto-upgrade.nix
@@ -1,12 +1,10 @@
 { cfg, lib, ... }:
 {
-  system.autoUpgrade = lib.mkIf (cfg.features.auto_update or false) {
-    enable = true;
-    allowReboot = cfg.features.auto_reboot or false;
+  system.autoUpgrade = lib.mkIf (cfg.features.autoUpdate or false) {
+    enable = cfg.features.autoUpdate or false;
+    allowReboot = cfg.features.autoReboot or false;
     flake = "github:guemidiborhane/nix-config";
     flags = [
-      "--recreate-lock-file"
-      "--no-write-lock-file"
       "-L"
     ];
     dates = "daily";
