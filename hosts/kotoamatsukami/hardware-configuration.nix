4d5e66292e28381f23edd09ba5d6a8df9ad4693a
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2026-02-01 16:41:50 +0100

    :ambulance: nix: put a band aid on it and act like it's not there

--- hosts/kotoamatsukami/hardware-configuration.nix
+++ hosts/kotoamatsukami/hardware-configuration.nix
@@ -4,14 +4,26 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "uas" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-  boot.kernelParams = [
-    "memtest=2"
-    "amd_pstate=active"
-  ];
+  boot = {
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "uas" "sd_mod" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+    kernelParams = [
+      "memmap=64K$0x0000000712f10000"
+      "memmap=32K$0x000000071b010000"
+      "memmap=32K$0x0000000726978000"
+      "memmap=8K$0x0000000446e76fb0"
+      "memmap=8K$0x00000007b14916b0"
+      "memmap=8K$0x00000007b28ad6b0"
+      "memmap=9180K$0x00000007b42517b0"
+      "memmap=8K$0x00000007ba118430"
+      "memmap=8K$0x00000007bbb697b0"
+
+      "memtest=4"
+      "amd_pstate=active"
+    ];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
