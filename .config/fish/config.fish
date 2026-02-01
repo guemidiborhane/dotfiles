70a6ef75995b7f093d587d0de1309f0c384f57ad
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-02-01 13:47:11 +0100

    :wrench: fish/tmux: support SSH agent forwarding in tmux
    - Only set SSH_AUTH_SOCK if not already defined (preserve forwarded
    agent)
    - Update SSH_AUTH_SOCK when reattaching tmux sessions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,7 @@ set -Ux ANSIBLE_NOCOWS true
 set -Ux BAT_THEME Dracula
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
 
-set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
+set -q SSH_AUTH_SOCK; or set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
 
 fish_add_path -mg "$HOME/.local/bin" "$HOME/.krew/bin"
 
