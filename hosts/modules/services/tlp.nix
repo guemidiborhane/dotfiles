95548e57f47848bdf05ad453310fa15d66077bb2
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-02-05 19:26:56 +0100

    :bug: nix/services: disable boost on battery

--- hosts/modules/services/tlp.nix
+++ hosts/modules/services/tlp.nix
@@ -5,13 +5,13 @@
     CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
     CPU_SCALING_GOVERNOR_ON_AC = "performance";
     PLATFORM_PROFILE_ON_AC = "balanced";
-    CPU_BOOST_ON_AC = true;
+    CPU_BOOST_ON_AC = 1;
     RADEON_DPM_STATE_ON_AC = "performance";
 
-    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
     PLATFORM_PROFILE_ON_BAT = "low-power";
-    CPU_BOOST_ON_BAT = false;
+    CPU_BOOST_ON_BAT = 0;
     RADEON_DPM_STATE_ON_BAT = "battery";
 
     START_CHARGE_THRESH_BAT0 = cfg.power.startChargeThreshold;
--- hosts/modules/user.nix
+++ hosts/modules/user.nix
@@ -1,11 +1,10 @@
 { pkgs, cfg, config, ... }:
 {
   users.groups.${cfg.user.username}.gid = 1000;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${cfg.user.username} = {
+    uid = 1000;
     isNormalUser = true;
     description = cfg.user.fullName;
-    uid = 1000;
     group = cfg.user.username;
     extraGroups = [ "networkmanager" "wheel" "uinput" ];
     shell = pkgs.unstable.${cfg.user.shell};
