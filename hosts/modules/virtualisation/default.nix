2dfdeb6c63b87600293f93b9b8088875a4e11a93
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-02-22 23:02:58 +0100

    :wrench: nix/virtualisation: run daemon as root

--- hosts/modules/virtualisation/default.nix
+++ hosts/modules/virtualisation/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [ ./containers.nix ];
 
   virtualisation.libvirtd = {
     enable = true;
-    qemu.runAsRoot = false; # TODO test extensively
+    qemu.runAsRoot = true;
   };
   programs.virt-manager.enable = true;
 }
