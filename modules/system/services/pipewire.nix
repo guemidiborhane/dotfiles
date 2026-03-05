ef77b48a19abee58314039d505d7368c7bfd155e
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-03-05 23:28:45 +0100

    :wrench: nix/services: add wireplumber output priorities

--- modules/system/services/pipewire.nix
+++ modules/system/services/pipewire.nix
@@ -12,7 +12,26 @@
         alsa.support32Bit = true;
         pulse.enable = true;
         wireplumber.enable = true;
+        wireplumber.extraConfig."10-audio-priority" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [ { "node.name" = "alsa_output.pci-0000_c4_00.6.HiFi__Speaker__sink"; } ];
+              actions."update-props"."priority.session" = 1000;
+            }
+            {
+              matches = [ { "node.name" = "alsa_output.pci-0000_c4_00.1.HiFi__HDMI2__sink"; } ];
+              actions."update-props"."priority.session" = 1500;
+            }
+          ];
+          "monitor.bluez.rules" = [
+            {
+              matches = [ { "node.name" = "~bluez_output_internal.*"; } ];
+              actions."update-props"."priority.session" = 2000;
+            }
+          ];
+        };
         jack.enable = true;
       };
+
     };
 }
