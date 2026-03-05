9da8a723d7d2dbe6e9ff425e1c0e226ff5f819fc
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2026-03-05 04:53:59 +0100

    :recycle: nix/nvidia: remove patch and switch back to stable

--- modules/system/hardware/nvidia.nix
+++ modules/system/hardware/nvidia.nix
@@ -1,32 +1,20 @@
-{ _, ... }:
+{ lib, ... }:
 {
   flake.nixosModules.hardware-nvidia =
-    { config, pkgs, ... }:
-    {
+    { config, h, ... }:
+    lib.mkIf h.hasNvidia {
       services.xserver.videoDrivers = [ "nvidia" ];
-      hardware.nvidia = {
-        open = true;
-        nvidiaSettings = true;
-        powerManagement.enable = true;
 
-        modesetting.enable = true;
-        package =
-          let
-            base = config.boot.kernelPackages.nvidiaPackages.latest;
-            cachyos-nvidia-patch = pkgs.fetchpatch {
-              url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
-              sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
-            };
+      hardware = {
+        graphics.enable = true;
 
-            # Patch the appropriate driver based on config.hardware.nvidia.open
-            driverAttr = if config.hardware.nvidia.open then "open" else "bin";
-          in
-          base
-          // {
-            ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
-              patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
-            });
-          };
+        nvidia = {
+          open = true;
+          nvidiaSettings = true;
+          powerManagement.enable = true;
+          modesetting.enable = true;
+          package = config.boot.kernelPackages.nvidiaPackages.stable;
+        };
       };
     };
 }
