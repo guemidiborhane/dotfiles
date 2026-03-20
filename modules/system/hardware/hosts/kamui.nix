a42e16d51cff63571e8dfd0d2e54b6a221e4929b
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-03-20 17:43:59 +0100

    :art: flake: format

--- modules/system/hardware/hosts/kamui.nix
+++ modules/system/hardware/hosts/kamui.nix
@@ -13,24 +13,28 @@
         (modulesPath + "/installer/scan/not-detected.nix")
         inputs.self.nixosModules.hardware-thinkfan
       ];
+      boot = {
+        kernelParams = [
+          "mem_sleep_default=s2idle"
+          "amdgpu.reset_method=4"
+        ];
+        kernelModules = [
+          "kvm-amd"
+          "amdgpu"
+        ];
+        extraModulePackages = [ ];
 
-      boot.initrd.availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "thunderbolt"
-        "usb_storage"
-        "sd_mod"
-      ];
-      boot.initrd.kernelModules = [ "dm-snapshot" ];
-      boot.kernelParams = [
-        "mem_sleep_default=s2idle"
-        "amdgpu.reset_method=4"
-      ];
-      boot.kernelModules = [
-        "kvm-amd"
-        "amdgpu"
-      ];
-      boot.extraModulePackages = [ ];
+        initrd = {
+          availableKernelModules = [
+            "nvme"
+            "xhci_pci"
+            "thunderbolt"
+            "usb_storage"
+            "sd_mod"
+          ];
+          kernelModules = [ "dm-snapshot" ];
+        };
+      };
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
