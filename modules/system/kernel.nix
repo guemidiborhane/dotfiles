22ae820361e0c82564feaa715b3c06344ae12def
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-04-15 07:54:38 +0100

    :wrench: nix/kernel: switch to LTO

--- modules/system/kernel.nix
+++ modules/system/kernel.nix
@@ -21,10 +21,10 @@
         linux-latest = pkgs.linuxPackages_latest;
         linux-zen = pkgs.linuxPackages_zen;
 
-        cachyos-latest = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-        cachyos-latest-v3 = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v3;
-        cachyos-latest-v4 = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v4;
-        cachyos-latest-zen4 = pkgs.cachyosKernels.linuxPackages-cachyos-latest-zen4;
+        cachyos-latest = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto;
+        cachyos-latest-v3 = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3;
+        cachyos-latest-v4 = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v4;
+        cachyos-latest-zen4 = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-zen4;
       };
     in
     {
