20e63ff3589dd1ee9af960587e65276ec97e3489
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-04-15 23:54:41 +0100

    :bug: nix/programs: fix neovim module being overridden

--- modules/system/programs/neovim.nix
+++ modules/system/programs/neovim.nix
@@ -6,11 +6,6 @@
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
-      configure.customRC = ''
-        set number
-        set relativenumber
-        set cursorline
-      '';
     };
   };
 }
