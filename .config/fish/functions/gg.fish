812f89d8b5a09a1b1527668653290bf3d4d0c55c
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-04-18 00:01:24 +0100

    :recycle: fish: add git function to fallback to yadm

--- .config/fish/functions/gg.fish
+++ .config/fish/functions/gg.fish
@@ -1,6 +1,6 @@
 function gg
     # Check if current directory is inside a git repository
-    if git rev-parse --git-dir >/dev/null 2>&1
+    if command git rev-parse --git-dir >/dev/null 2>&1 || not string match -q "/home/*" $PWD
         # Launch lazygit in current git repo
         tmux_popup -w 90% -h 90% lazygit
     else
