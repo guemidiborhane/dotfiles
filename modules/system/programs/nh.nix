d6d7a0c78fe3cb63a3b7a8bfa5714446bfd651fa
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2026-04-20 15:06:25 +0100

    :wrench: nix: update nh-clean configs to lower impact on IO at boot

--- modules/system/programs/nh.nix
+++ modules/system/programs/nh.nix
@@ -1,14 +1,27 @@
-{ _, ... }:
+{ lib, ... }:
 {
   flake.modules.nixos.programs-nh = {
     programs.nh = {
       enable = true;
       clean = {
         enable = true;
-        dates = "daily";
+        dates = "monthly";
         extraArgs = "--keep-since 7d --keep 3";
       };
       flake = "/etc/nixos";
+    };
+    systemd.timers.nh-clean = {
+      timerConfig = {
+        Persistent = lib.mkForce false; # don't catch up on missed runs
+      };
+    };
+    systemd.services.nh-clean = {
+      serviceConfig = {
+        IOSchedulingClass = "idle";
+        IOSchedulingPriority = 7;
+        CPUSchedulingPolicy = "idle";
+        Nice = 19;
+      };
     };
   };
 }
