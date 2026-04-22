47c8cdc6496108a4396f920b8133227168be86c4
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-04-22 08:25:24 +0100

    :recycle: fish: secure_random print to STDOUT and copy

--- .config/fish/functions/secure_random.fish
+++ .config/fish/functions/secure_random.fish
@@ -1,6 +1,6 @@
 function secure_random --argument size
     set -q size[1]; or set size 64
-    tr -cd '[:alnum:]' </dev/urandom | fold -w $size | head -n 1 | tr -d '\n' | clip
+    tr -cd '[:alnum:]' </dev/urandom | fold -w $size | head -n 1 | tr -d '\n' | tee /dev/tty | clip
 end
 
 # vim: ft=fish
