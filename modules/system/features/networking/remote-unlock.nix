eb4de1ee671528690682b9962d8adb82a1a48a0f
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2026-04-27 20:52:47 +0100

    :bug: nix/features: fix remote-unlock for stage 1 systemd

--- modules/system/features/networking/remote-unlock.nix
+++ modules/system/features/networking/remote-unlock.nix
@@ -3,9 +3,18 @@
   flake.modules.nixos.features-remote-unlock =
     { features, ... }:
     lib.mkIf (features.remoteUnlock or false) {
-      boot.kernelParams = [ "ip=dhcp" ];
       boot.initrd = {
         availableKernelModules = [ "r8169" ];
+        systemd = {
+          network = {
+            enable = true;
+            networks."10-lan" = {
+              enable = true;
+              matchConfig.Name = "enp*";
+              networkConfig.DHCP = "yes";
+            };
+          };
+        };
         network = {
           enable = true;
           flushBeforeStage2 = true;
@@ -16,8 +25,8 @@
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAizWh/D3fQY7CT8onqVH4M0BJ7l5fpBsdCPvx3TsUo/ luks@warden"
             ];
             hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+            shell = "/usr/bin/systemd-tty-ask-password-agent";
           };
-          postCommands = "";
         };
       };
     };
