f4d5e7c7ff75ac828bd4089bad9f587e48591e2f
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-05-13 19:19:21 +0100

    :fire: nix: remove pre/post hibernate services

--- modules/system/features/swap/zram.nix
+++ modules/system/features/swap/zram.nix
@@ -14,41 +14,5 @@
         algorithm = "lz4";
         memoryPercent = 30;
       };
-
-      systemd.services.hibernate-zram-pre = {
-        description = "Disable ZRAM before hibernation";
-        before = [
-          "systemd-hibernate.service"
-          "systemd-suspend-then-hibernate.service"
-        ];
-        wantedBy = [
-          "systemd-hibernate.service"
-          "systemd-suspend-then-hibernate.service"
-        ];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.util-linux}/bin/swapoff /dev/zram0";
-          RemainAfterExit = true;
-        };
-      };
-
-      # kinda redundant but better safe than sorry, already handled by systemd-zram-setup@zram0.service
-      systemd.services.hibernate-zram-post = {
-        description = "Re-enable ZRAM after resume from hibernation";
-        after = [
-          "hibernate.target"
-          "suspend-then-hibernate.target"
-        ];
-        wantedBy = [
-          "hibernate.target"
-          "suspend-then-hibernate.target"
-        ];
-        serviceConfig = {
-          Type = "oneshot";
-          # Ignore exit code 255 (already active)
-          ExecStart = "${pkgs.util-linux}/bin/swapon --priority 100 /dev/zram0";
-          SuccessExitStatus = "0 255";
-        };
-      };
     };
 }
