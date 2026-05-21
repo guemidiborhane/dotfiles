f190fbbcecefb81adba7e1b18e2b263a0ab384db
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-05-21 02:23:12 +0100

    :recycle: fish: extract env to conf.d, normalize scopes, add load-order prefixes
    - move env vars from config.fish into conf.d/00_env.fish for earlier sourcing in login shells and a single authoritative source
    - normalize all vars to -gx; drop -Ux to avoid hidden state in fish_variables outside dotfiles control
    - rename conf.d files with numeric prefixes (00_/10_/90_) to make load order explicit
    - simplify 10_startx: drop dead script-path guard, exec start-hyprland directly

---
+++ .config/fish/conf.d/00_env.fish
@@ -0,0 +1,17 @@
+# vi: set ft=fish :
+
+set -gx EDITOR nvim
+set -gx VISUAL $EDITOR
+set -gx SUDO_EDITOR $EDITOR
+
+set -gx LESS -rF
+set -gx PAGER less -R
+set -gx MANPAGER nvim +Man!
+
+set -gx ANSIBLE_NOCOWS true
+set -gx BAT_THEME Dracula
+set -gx FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --reverse"
+
+set -q SSH_AUTH_SOCK; or set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
+
+fish_add_path -mg "$HOME/.local/bin" "$HOME/.krew/bin"
