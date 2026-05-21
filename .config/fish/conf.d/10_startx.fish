8ef080eefa0ea1d09b06c29973053ac2414f1dd2
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-05-21 02:23:12 +0100

    :recycle: fish: extract env to conf.d, normalize scopes, add load-order prefixes
    - move env vars from config.fish into conf.d/00_env.fish for earlier sourcing in login shells and a single authoritative source
    - normalize all vars to -gx; drop -Ux to avoid hidden state in fish_variables outside dotfiles control
    - rename conf.d files with numeric prefixes (00_/10_/90_) to make load order explicit
    - simplify 10_startx: drop dead script-path guard, exec start-hyprland directly

--- .config/fish/conf.d/10_startx.fish
+++ .config/fish/conf.d/10_startx.fish
@@ -1,8 +1,6 @@
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1 -a -z "$SSH_CONNECTION" -a (tty | string match -r '/dev/tty[0-9]+')
-        if test -f "$script"
-            exec start-hyprland
-        end
+        exec start-hyprland
     end
 end
 
