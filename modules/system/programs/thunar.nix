4a437982480f35d0f8d8eb4a5220db1f03a0b225
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-07-11 19:16:00 +0100

    :wrench: thunar: add images thumbnails
    From the official wiki

--- modules/system/programs/thunar.nix
+++ modules/system/programs/thunar.nix
@@ -3,6 +3,9 @@
   flake.modules.nixos.thunar =
     { pkgs, ... }:
     {
+      programs.xfconf.enable = true;
+      services.gvfs.enable = true; # Mount, trash, and other functionalities
+      services.tumbler.enable = true; # Thumbnail support for images
       programs.thunar = {
         enable = true;
         plugins = with pkgs.unstable; [
