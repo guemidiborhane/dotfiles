f04c0836a3d6c590e0f1ce1c1078ab90fcc93cf8
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-07-12 05:35:44 +0100

    :recycle: features: set default command for remote-unlock per key
    This follows the official wiki instructions

--- modules/system/features/networking/remote-unlock.nix
+++ modules/system/features/networking/remote-unlock.nix
@@ -7,14 +7,13 @@
         kernelParams = [ "ip=dhcp" ];
         initrd = {
           availableKernelModules = [ "r8169" ];
-          systemd.users.root.shell = "/usr/bin/systemd-tty-ask-password-agent";
           network = {
             enable = true;
             ssh = {
               enable = true;
               port = 22;
               authorizedKeys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAizWh/D3fQY7CT8onqVH4M0BJ7l5fpBsdCPvx3TsUo/ luks@warden"
+                ''command="systemctl default" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAizWh/D3fQY7CT8onqVH4M0BJ7l5fpBsdCPvx3TsUo/ luks@warden''
               ];
               hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
             };
