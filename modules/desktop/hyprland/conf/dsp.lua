a781db26e97309ba11dcf55bdd3c95f5219af867
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2026-07-24 08:10:19 +0100

    :recycle: hypr: move handlers declaration into vars

--- modules/desktop/hyprland/conf/dsp.lua
+++ modules/desktop/hyprland/conf/dsp.lua
@@ -1,8 +1,7 @@
 local v = require("lua.vars")
 
-local M = {}
-
-M.dispatchers = {
+local M = {
+  handlers = {},
   add = function(self, prefix, handler)
     local p = prefix .. ":"
     if self.handlers[p] then error(prefix .. " handler prefix already registered") end
@@ -13,7 +12,7 @@ M.dispatchers = {
   get = function(self, cmd, opts)
     for prefix, d in next, self.handlers do
       if cmd:sub(1, #prefix) == prefix then
-        return d(cmd:sub(#prefix + 1), opts)
+        return d(cmd:sub(#prefix + 1), opts, self)
       end
     end
 
@@ -22,40 +21,15 @@ M.dispatchers = {
 }
 
 function M.cmd(cmd, opts)
-  return M.dispatchers:get(cmd, opts)
+  return M:get(cmd, opts)
 end
 
 function M.exec(cmd, opts)
   return hl.dsp.exec_raw(M.cmd(cmd, opts))
 end
 
-M.dispatchers.handlers = {
-  ["sh:"] = function(cmd, _)
-    return string.format("%s -c %q", os.getenv("SHELL"), cmd)
-  end,
-
-  ["app:"] = function(exec, slice)
-    local cmd = "hydrun --prefix hypr"
-    if slice then cmd = string.format("%s --slice=%q", cmd, slice) end
-
-    return string.format("%s -- %s", cmd, exec)
-  end,
-
-  ["term:"] = function(cmd, app_id)
-    local command = v.apps.terminal.main
-    if app_id then command = string.format("%s --app-id %q", command, app_id) end
-
-    return string.format("%s -e %s", M.dispatchers:get(command), cmd)
-  end,
-
-  ["popup:"] = function(cmd, _)
-    return M.dispatchers:get("term:" .. cmd, "terminal.popup")
-  end,
-
-  ["tmux:"] = function(session_name)
-    local cmd = string.format("tmux new-session -As %q", session_name)
-    return M.dispatchers:get("term:" .. cmd, session_name .. "-session")
-  end
-}
+for prefix, handler in next, v.handlers do
+  M:add(prefix, handler)
+end
 
 return M
--- modules/desktop/hyprland/conf/vars.lua
+++ modules/desktop/hyprland/conf/vars.lua
@@ -1,4 +1,4 @@
-return {
+local M = {
   mods = {
     Meta = "SUPER",
     Shift = "SHIFT",
@@ -31,3 +31,34 @@ return {
     { class = "legcord" },
   }
 }
+
+M.handlers = {
+  sh = function(cmd, _, _)
+    return string.format("%s -c %q", os.getenv("SHELL"), cmd)
+  end,
+
+  app = function(exec, slice, _)
+    local cmd = "hydrun --prefix hypr"
+    if slice then cmd = string.format("%s --slice=%q", cmd, slice) end
+
+    return string.format("%s -- %s", cmd, exec)
+  end,
+
+  term = function(cmd, app_id, dispatch)
+    local command = M.apps.terminal.main
+    if app_id then command = string.format("%s --app-id %q", command, app_id) end
+
+    return string.format("%s -e %s", dispatch:get(command), cmd)
+  end,
+
+  popup = function(cmd, _, dispatch)
+    return dispatch:get("term:" .. cmd, "terminal.popup")
+  end,
+
+  tmux = function(session_name, _, dispatch)
+    local cmd = string.format("tmux new-session -As %q", session_name)
+    return dispatch:get("term:" .. cmd, session_name .. "-session")
+  end,
+}
+
+return M
