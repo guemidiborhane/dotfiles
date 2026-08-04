6daa3c03b8f425e1e4b8083b952099e93042d7a0
Borhaneddine GUEMIDI <guemidiborhane@aina.dz>
2026-08-04 19:24:32 +0100

    :recycle: adguard: split filters to declutter config

---
+++ modules/system/features/services/adguard/_filters.nix
@@ -0,0 +1,77 @@
+[
+  {
+    id = 1;
+    name = "AdGuard DNS filter";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+  }
+  {
+    id = 1752367266;
+    name = "AdGuard DNS Popup Hosts filter";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_59.txt";
+  }
+  {
+    id = 1752367267;
+    name = "Dan Pollock's List";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_4.txt";
+  }
+  {
+    id = 1752367268;
+    name = "HaGeZi's Ultimate Blocklist";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_49.txt";
+  }
+  {
+    id = 1752367269;
+    name = "OISD Blocklist Big";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt";
+  }
+  {
+    id = 1752367270;
+    name = "Peter Lowe's Blocklist";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_3.txt";
+  }
+  {
+    id = 1752367271;
+    name = "Steven Black's List";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt";
+  }
+  {
+    id = 1752367273;
+    name = "Dandelion Sprout's Anti-Malware List";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_12.txt";
+  }
+  {
+    id = 1752367274;
+    name = "HaGeZi's Badware Hoster Blocklist";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_55.txt";
+  }
+  {
+    id = 1752367276;
+    name = "HaGeZi's Threat Intelligence Feeds";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_44.txt";
+  }
+  {
+    id = 1752367277;
+    name = "Phishing Army";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_18.txt";
+  }
+  {
+    id = 1752367278;
+    name = "ShadowWhisperer's Malware List";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_42.txt";
+  }
+  {
+    id = 1752367279;
+    name = "Stalkerware Indicators List";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_31.txt";
+  }
+  {
+    id = 1752367280;
+    name = "uBlock₀ filters – Badware risks";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_50.txt";
+  }
+  {
+    id = 1752367281;
+    name = "Malicious URL Blocklist (URLHaus)";
+    url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt";
+  }
+]
---
+++ modules/system/features/services/adguard/adguard.nix
@@ -0,0 +1,56 @@
+{ _, ... }:
+{
+  flake.modules.nixos.adguard =
+    { lib, features, secrets, ... }:
+    let
+      port = 5353;
+    in
+    lib.mkIf (features.adguard or false) {
+      networking.nameservers = [ "127.0.0.1:${toString port}" ];
+
+      services.adguardhome = {
+        enable = true;
+        port = 8053;
+
+        settings = {
+          dns = {
+            inherit port;
+            bind_hosts = [ "127.0.0.1" ];
+
+            upstream_mode = "parallel";
+            upstream_dns = [
+              "https://security.cloudflare-dns.com/dns-query"
+              "tls://security.cloudflare-dns.com"
+              "https://dns.quad9.net/dns-query"
+              "tls://dns.quad9.net"
+            ];
+            bootstrap_dns = [
+              "9.9.9.10"
+              "149.112.112.10"
+              "2620:fe::10"
+              "2620:fe::fe:10"
+            ];
+            fallback_dns = [
+              "1.1.1.2"
+              "1.0.0.2"
+            ];
+          };
+
+          filters = map (filter: {
+            enabled = true;
+            inherit (filter) id name url;
+          }) (import ./_filters.nix);
+
+          filtering.rewrites = [
+            {
+              domain = "freedium.cfd";
+              answer = "146.103.108.112";
+              enabled = true;
+            }
+          ];
+
+          user_rules = secrets.adguard.user_rules;
+        };
+      };
+    };
+}
