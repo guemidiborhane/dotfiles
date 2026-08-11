c30e43766f0f5df72fb0f8f736abfd8e22eec2d0
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2026-08-11 02:19:14 +0100

    :arrow_up: nix: system update
    CHANGED
    [U.] aardvark-dns                          2.0.0 -> 2.1.0, -22.5 KiB
    [U.] audit                                 4.1.4-lib, <unchanged> -> 4.2.1-lib, <unchanged>
    [U.] equicord                              1.15.0.2-2026-08-01 -> 1.15.1.0-2026-08-10, +9.06 KiB
    [D.] gssdp                                 1.6.4, 1.6.6 -> 1.6.6 ×2
    [D.] gupnp                                 1.6.9, 1.6.10 -> 1.6.10 ×2
    [U.] helium                                0.15.1.1-bwrap, 0.15.1.1-extracted, 0.15.1.1-fhsenv-profile, 0.15.1.1-fhsenv-rootfs, 0.15.1.1-fish-completions, 0.15.1.1-init, 0.15.1.1 -> 0.15.3.1-bwrap, 0.15.3.1-extracted, 0.15.3.1-fhsenv-profile, 0.15.3.1-fhsenv-rootfs, 0.15.3.1-fish-completions, 0.15.3.1-init, 0.15.3.1, +229 KiB
    [C.] index-x86_64-linux                    +1.35 MiB
    [C.] index-x86_64-linux-small              +13.2 KiB
    [U.] lazygit                               0.63.1-fish-completions, 0.63.1 -> 0.64.0-fish-completions, 0.64.0, +225 KiB
    [D*] libffi                                3.7.0, 3.7.1 ×2, <unchanged> -> 3.7.1 ×3, <unchanged>
    [D.] libssh                                0.12.0, 0.12.1 ×2 -> 0.12.1 ×3
    [U.] mesa                                  26.1.6 ×2 -> 26.2.0 ×2, +13.9 MiB
    [C*] nixos-configuration-reference-manpage +14.8 KiB
    [C*] nixos-manual-html                     +185 KiB
    [U.] nixos-system-kamui                    26.11.20260804.e72e4f2 ×2 -> 26.11.20260807.f13ff45 ×2
    [C.] noctalia                              +396 KiB
    [C.] python3                               -15.2 KiB
    [U*] qemu                                  11.0.2-doc, 11.0.2_fish-completions, 11.0.2-ga, 11.0.2 -> 11.0.3-doc, 11.0.3_fish-completions, 11.0.3-ga, 11.0.3, +133 KiB
    [C.] qtdeclarative                         -117 KiB
    [D.] s2n-tls                               1.7.2, 1.7.6 -> 1.7.6 ×2, +23.5 KiB
    [C.] source                                +307 KiB
    [U.] tpm2-tss                              4.1.3 ×4 -> 4.2.0, 4.1.3 ×3, +120 KiB
    [D.] unbound                               1.25.1-lib, 1.25.2-lib ×2, <unchanged> -> 1.25.2-lib ×3, <unchanged>
    [U.] uosc                                  5.12.0 -> 5.13.0, +13.6 KiB
    [U.] uosc-bin                              5.12.0 -> 5.13.0
    [U.] vicinae                               0.24.0-configured-fish-completions, 0.24.0-configured, 0.24.0 -> 0.25.0-configured-fish-completions, 0.25.0-configured, 0.25.0, +404 KiB
    [U.] vte                                   0.84.0 -> 0.84.1
    [U.] x86_energy_perf_policy                6.18.42 -> 6.18.43
    [U*] xdg-desktop-portal-hyprland           1.4.1+date=2026-08-04_b653ab5, <unchanged> -> 1.4.1+date=2026-08-06_688feb3, <unchanged>
    [U.] zen-beta                              1.21.10b-fish-completions, 1.21.10b -> 1.21.13b-fish-completions, 1.21.13b
    [U.] zen-beta-bin-unwrapped                1.21.10b -> 1.21.13b, +97.1 KiB
    
    PATHS: 3576 -> 3576 (+645, -645)
    SIZE: 27.3 GiB -> 27.3 GiB
    DIFF: +17.2 MiB

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -7,21 +7,7 @@
     # This one contains whatever you want to overlay
     # You can change versions, add patches, set compilation flags, anything really.
     # https://nixos.wiki/wiki/Overlays
-    modifications = final: prev: {
-      # TODO: drop once
-      # https://github.com/NixOS/nixpkgs/pull/549253
-      # lands on nixos-unstable
-      hyprland = prev.hyprland.overrideAttrs (oldAttrs: {
-        postPatch = ''
-          # Relax glaze dependency
-          # FIXME: this shouldn't be needed once the upstream code will adopt it
-          substituteInPlace CMakeLists.txt start/CMakeLists.txt hyprpm/CMakeLists.txt \
-            --replace-fail "glaze 7...<8" "glaze"
-
-        ''
-        + (oldAttrs.postPatch or "");
-      });
-    };
+    modifications = final: prev: { };
 
     # When applied, the unstable nixpkgs set (declared in the flake inputs) will
     # be accessible through 'pkgs.unstable'
